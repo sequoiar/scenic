76c23b4562632aebfcc87e2a0da6e84e1aeb2f16
Koya Charles <koya.charles@gmail.com>
2008-04-29 19:36:14 +0000

    specified h264

--- gstreamer/shellScripts/rtp-h264Sender.sh
+++ gstreamer/shellScripts/rtp-h264Sender.sh
@@ -1,7 +1,6 @@
-if [ $# = 1 ]; then
-    port=10010
-    gst-launch-0.10 dv1394src ! dvdemux ! dvdec ! ffmpegcolorspace ! x264enc byte-stream=true threads=4 ! rtph264pay ! udpsink host=$1 port=$port
+if [ $# = 2 ]; then
+    gst-launch-0.10 dv1394src ! dvdemux ! dvdec ! ffmpegcolorspace ! x264enc byte-stream=true threads=4 ! rtph264pay ! udpsink host=$1 port=$2
 else
-    echo "$0" requires host ip
+    echo "$0" requires host ip and port
 fi
 
--- gstreamer/shellScripts/rtpReceiver.sh
+++
@@ -1,3 +0,0 @@
-gst-launch-0.10 udpsrc port=10010 caps="application/x-rtp" ! rtph264depay \ 
-! ffdec_h264 ! xvimagesink sync=false
-
