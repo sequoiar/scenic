5c676e373d293c69945b62e682bdd82ba6dc3a60
Koya Charles <koya.charles@gmail.com>
2008-04-29 19:46:35 +0000

    better names

--- gstreamer/shellScripts/rtp-h264Sender.sh
+++ gstreamer/shellScripts/rtp-h264Sender.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if [ $# = 2 ]; then
     gst-launch-0.10 dv1394src ! dvdemux ! dvdec ! ffmpegcolorspace ! x264enc byte-stream=true threads=4 ! rtph264pay ! udpsink host=$1 port=$2
 else
