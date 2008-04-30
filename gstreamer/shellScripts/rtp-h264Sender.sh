d80fd2c2a2e6d6a3cdaa3aa23fabbc145a7ae094
Koya Charles <koya.charles@gmail.com>
2008-04-30 17:18:15 +0000

    dv or v4l

--- gstreamer/shellScripts/rtp-h264Sender.sh
+++ gstreamer/shellScripts/rtp-h264Sender.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-if [ $# = 2 ]; then
-    gst-launch-0.10 dv1394src ! dvdemux ! dvdec ! ffmpegcolorspace ! x264enc byte-stream=true threads=4 ! rtph264pay ! udpsink host=$1 port=$2
+if [ $# = 3 ]; then
+    if [ $1 = "dv" ]; then
+        gst-launch-0.10 dv1394src ! dvdemux ! dvdec ! ffmpegcolorspace ! x264enc bitrate=2000 byte-stream=true threads=4 ! rtph264pay ! udpsink host=$2 port=$3
+    else
+        gst-launch-0.10 v4l2src ! ffmpegcolorspace ! x264enc bitrate=2000 byte-stream=true threads=4 ! rtph264pay ! udpsink host=$2 port=$3
+    fi
 else
-    echo "$0" requires host ip and port
+    echo "$0" requires dv or v4l  - host ip and port 
 fi
 
