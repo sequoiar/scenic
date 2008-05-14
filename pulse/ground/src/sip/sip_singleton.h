0b21a622b59bdffafe51218a24c98d63e6cfd456
Koya Charles <koya.charles@gmail.com>
2008-05-14 21:52:44 +0000

    comments sip_singleton.h

--- pulse/ground/src/sip/sip_singleton.h
+++ pulse/ground/src/sip/sip_singleton.h
@@ -4,7 +4,12 @@
 class sip_singleton{
 public:
     static sip_singleton* Instance();
+
+    //called inside pjsip rx_request 
+    //must return pchar to desired response
     char *rx_req(void *data,unsigned int len );
+    
+    //called inside pjsip rx_response
     void rx_res(void *data,unsigned int len );
 private:
     sip_singleton(){};
