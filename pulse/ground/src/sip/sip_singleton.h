fc3db1a85595b1a400c32eee60c5e611f100758a
Tristan Matthews <tristan@sat.qc.ca>
2008-05-14 22:06:11 +0000

    naming conventions fixed

--- pulse/ground/src/sip/sip_singleton.h
+++ pulse/ground/src/sip/sip_singleton.h
@@ -1,22 +1,23 @@
 #ifndef _SIP_SINGLETON_H_
 #define _SIP_SINGLETON_H_
 
-class sip_singleton{
+class SipSingleton
+{
 public:
-    static sip_singleton* Instance();
+    static SipSingleton* Instance();
 
     //called inside pjsip rx_request 
     //must return pchar to desired response
-    char *rx_req(void *data,unsigned int len );
+    char *rx_req(void *data, unsigned int len);
     
     //called inside pjsip rx_response
-    void rx_res(void *data,unsigned int len );
-private:
-    sip_singleton(){};
+    void rx_res(void *data, unsigned int len);
 
+private:
+    SipSingleton(){};
 
     // Incomming data -> char* response
-    static sip_singleton *s;
+    static SipSingleton *s;
 };
 
 #endif
