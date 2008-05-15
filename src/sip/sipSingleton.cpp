0c7636afa5f06055eed921189671665873d9aa0b
Koya Charles <koya.charles@gmail.com>
2008-05-15 22:06:27 +0000

    main now refers only to SipSingleton

--- src/sip/sipSingleton.cpp
+++ src/sip/sipSingleton.cpp
@@ -39,26 +39,48 @@ SipSingleton* SipSingleton::Instance()
     return s;
 }
 
+void SipSingleton::send_request(const char* msg)
+{
+    ::send_request(msg);
+
+}
+
+int SipSingleton::handle_events(void)
+{
+    return ::sip_handle_events();
+}
+
+bool SipSingleton::init(int argc, char* argv[])
+{
+    if(sip_pass_args(argc,argv) < 0)
+        return false;
+
+    sip_init();
+    return true;
+}
+
+
+
+
 
 
 int main(int argc, char *argv[])
 {
+    SipSingleton &sip = *SipSingleton::Instance();
 
-    if(sip_pass_args(argc,argv) < 0)
+    if(!sip.init(argc,argv))
         return -1;
-
-    sip_init();
 
     if (argc == 5)
     {
-        send_request((char*)"Hello World");
+        sip.send_request("Hello World");
     }
 
 
     for (;;)
     {
         static int eventCount;
-        if (eventCount += sip_handle_events())
+        if (eventCount += sip.handle_events())
         {
             std::cout << "HANDLED " << eventCount << " EVENTS " << std::endl;
         }
