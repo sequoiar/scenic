8bcf6dca5e0052e0753cbd965769613df5c4b5e2
Koya Charles <koya.charles@gmail.com>
2008-05-16 15:50:32 +0000

    send service request receive port

--- pulse/ground/src/sip/sipPrivate.h
+++ pulse/ground/src/sip/sipPrivate.h
@@ -7,4 +7,6 @@ unsigned int sip_handle_events(void);
 int sip_init(void);
 void send_request(const char *str);
 
+void  sip_set_local(const char* host, const char* port);
+void  sip_set_remote(const char* host, const char* port);
 #endif
