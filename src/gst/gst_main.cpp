f3ab5059ab929b49725dd4fbe6d37a9f01f995e6
Tristan Matthews <tristan@sat.qc.ca>
2008-05-16 16:25:40 +0000

    tester example code in comments, not used yet

--- src/gst/gst_main.cpp
+++ src/gst/gst_main.cpp
@@ -1,6 +1,7 @@
 
 // main_entry.c
 #include "../sip/sipSingleton.h"
+#include "../sip/sipTester.h"
 
 
 
@@ -11,6 +12,9 @@ int main(int argc, char *argv[])
 {
     SipSingleton &sip = *SipSingleton::Instance();
     
+    // SipTester client(sip);
+    // client.create_session();
+    // client.send_messages();
     sip.set_service_port(10010);
 
     sip.init("192.168.1.164","5060","192.168.1.164","5061");
