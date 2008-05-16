5dd414837333da8d41f30abff984df1dcd75c7d0
Koya Charles <koya.charles@gmail.com>
2008-05-16 16:04:30 +0000

    linked in pjsip to gst

--- pulse/ground/src/gst/gst_main.cpp
+++ pulse/ground/src/gst/gst_main.cpp
@@ -1,10 +1,23 @@
+
 // main_entry.c
+#include "../sip/sipSingleton.h"
+
+
 
 int gst_main(int argc, char *argv[]);
     
 
 int main(int argc, char *argv[])
 {
+    SipSingleton &sip = *SipSingleton::Instance();
+    
+    sip.set_service_port(10010);
+
+    sip.init("192.168.1.164","5060","192.168.1.164","5061");
+
+    sip.send_request("h264.1");
+
+
     return gst_main(argc,argv);
 }
 
