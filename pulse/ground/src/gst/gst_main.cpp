1b91dd73ba81a503dbe9f63b94d0b2940a7edcd0
Koya Charles <koya.charles@gmail.com>
2008-05-16 19:10:58 +0000

    pjsip & gst

--- pulse/ground/src/gst/gst_main.cpp
+++ pulse/ground/src/gst/gst_main.cpp
@@ -1,27 +1,10 @@
-
 // main_entry.c
-#include "../sip/sipSingleton.h"
-#include "../sip/sipTester.h"
-
-
 
 int gst_main(int argc, char *argv[]);
     
 
 int main(int argc, char *argv[])
 {
-    SipSingleton &sip = *SipSingleton::Instance();
-    
-    // SipTester client(sip);
-    // client.create_session();
-    // client.send_messages();
-    sip.set_service_port(10010);
-
-    sip.init("192.168.1.164","5060","192.168.1.164","5061");
-
-    sip.send_request("h264.1");
-
-
     return gst_main(argc,argv);
 }
 
