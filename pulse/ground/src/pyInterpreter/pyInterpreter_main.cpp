6549d12783590ed6a830aa752519dd12ab3d6b92
Koya Charles <koya.charles@gmail.com>
2008-06-09 19:15:54 +0000

    GPL and file comments

--- pulse/ground/src/pyInterpreter/pyInterpreter_main.cpp
+++ pulse/ground/src/pyInterpreter/pyInterpreter_main.cpp
@@ -1,4 +1,3 @@
-
 // pyInterpreter.h
 // Copyright 2008 Koya Charles & Tristan Matthews 
 //     
@@ -18,6 +17,13 @@
 // along with [propulse]ART.  If not, see <http://www.gnu.org/licenses/>.
 //
 //
+
+/** \file
+ *      Main python loop
+ *
+ *      _ground_loop() called on every return\n
+ *      See python_import.cpp
+ */
 
 #include "pyInterpreter.h"
 
