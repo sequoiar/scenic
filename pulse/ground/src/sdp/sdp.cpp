560a7245fd1c549dd8096c89ff58cca02dc6b815
Koya Charles <koya.charles@gmail.com>
2008-06-09 19:03:46 +0000

    GPL licence - replaced const for immutable sdpmedia vars

--- pulse/ground/src/sdp/sdp.cpp
+++ pulse/ground/src/sdp/sdp.cpp
@@ -1,3 +1,28 @@
+// sdp.cpp
+// Copyright 2008 Koya Charles & Tristan Matthews 
+//     
+// This file is part of [propulse]ART.
+//
+// [propulse]ART is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// [propulse]ART is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with [propulse]ART.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+/** \file 
+ *      Implemets SDP protocol
+ *      Classes Sdp, SdpMedia, SdpMediaFactory
+ */
+
+
 #include "sdp.h"
 
 
@@ -16,7 +41,7 @@ bool Sdp::add_media(SdpMedia _m)
     return 1;
 
 }
-/*
+
 SdpMedia& SdpMedia::operator=(const SdpMedia& m)
 { 
     if(&m == this)
@@ -29,7 +54,7 @@ SdpMedia& SdpMedia::operator=(const SdpMedia& m)
 
     return *this;
 }
-*/
+
 std::string SdpMedia::str()
 {
     std::stringstream ret;
