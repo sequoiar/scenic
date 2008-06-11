a2119f25350c10f34160ef5d0b61d2bdaaad9af0
Tristan Matthews <tristan@sat.qc.ca>
2008-06-11 21:09:13 +0000

    gstrtpbin stuff still broken, offensive lines commented out for now

--- prototypes/gstreamer/shellScripts/udpSendInterleave.sh
+++ prototypes/gstreamer/shellScripts/udpSendInterleave.sh
@@ -1,4 +1,4 @@
-gst-launch-0.10 -v interleave name=i ! audioconvert ! vorbisenc ! rtpvorbispay ! udpsink host=localhost port=5060 \
+gst-launch-0.10 -v interleave name=i ! audioconvert ! "audio/x-raw-float, channels=8" ! vorbisenc ! rtpvorbispay ! udpsink host=localhost port=5060 \
 audiotestsrc volume=0.5 freq=200 is-live=false ! audioconvert ! queue ! i. \
 audiotestsrc volume=0.5 freq=300 is-live=false ! audioconvert ! queue ! i. \
 audiotestsrc volume=0.1 freq=400 is-live=false ! audioconvert ! queue ! i. \
@@ -6,4 +6,4 @@ audiotestsrc volume=0.1 freq=500 is-live=false ! audioconvert ! queue ! i. \
 audiotestsrc volume=0.1 freq=600 is-live=false ! audioconvert ! queue ! i. \
 audiotestsrc volume=0.1 freq=700 is-live=false ! audioconvert ! queue ! i. \
 audiotestsrc volume=0.1 freq=800 is-live=false ! audioconvert ! queue ! i. \
-audiotestsrc volume=0.1 freq=900 is-live=false ! audioconvert ! queue ! i. 
+audiotestsrc volume=0.1 freq=900 is-live=false !  audioconvert ! queue ! i. 
--- prototypes/gstreamer/shellScripts/udpSendStereo.sh
+++ prototypes/gstreamer/shellScripts/udpSendStereo.sh
@@ -1,3 +1,3 @@
-gst-launch-0.10 -v interleave name=i ! audioconvert ! vorbisenc ! rtpvorbispay ! udpsink host=localhost port=5060 \
+gst-launch-0.10 -v interleave name=i ! vorbisenc ! rtpvorbispay ! udpsink host=localhost port=5060 \
 audiotestsrc volume=0.5 freq=200 is-live=false ! audioconvert ! queue ! i. \
 audiotestsrc volume=0.5 freq=400 is-live=false ! audioconvert ! queue ! i. 
