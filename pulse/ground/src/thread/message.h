382f321402c0ff8d6d6af62461bfcb2f458606ce
Koya Charles <koya.charles@gmail.com>
2008-06-26 22:01:35 +0000

    message

---
+++ pulse/ground/src/thread/message.h
@@ -0,0 +1,23 @@
+#ifndef __MESSAGE__
+#define __MESSAGE__
+namespace message {
+    enum type 
+    { 
+        undefined, err, ok, ack, open, close, start, stop, pause, quit, info 
+    };
+    const char* str[] = 
+    { 
+        "undefined","err","ok","ack","open","close","start","stop","pause","quit","info" 
+    };
+};
+
+struct Message
+{
+	Message(int m):type((message::type)(m)){}                    
+	Message(message::type m):type(m){}                    
+    Message():type(message::undefined){}
+    message::type type;
+    int getInt(){return (int)type;}
+};
+
+#endif
