bd1c6b8ff9b4d0e75b399da04ae8bbeaba9f41ab
Koya Charles <koya.charles@gmail.com>
2008-07-02 22:56:17 +0000

    indent stuff

--- src/hello/hello.cpp
+++ src/hello/hello.cpp
@@ -1,16 +1,15 @@
 #include "hello.h"
 
-const char * Hello::greet()
+const char *Hello::greet()
 {
     static std::string ts;
+
     ts = "hello " + s;
-    
+
     return ts.c_str();
 }
 
-void Hello::set_name(char const* n)
+void Hello::set_name(char const *n)
 {
     s = n;
 }
-
-
