9bac8411ffab7dfbc3ec88f4bcb82ca2f86fa011
Koya Charles <koya.charles@gmail.com>
2008-07-07 19:31:23 +0000

    args tests

---
+++ src/main/test/main.cpp
@@ -0,0 +1,42 @@
+// headerGPL.c
+// Copyright 2008 Koya Charles & Tristan Matthews
+//
+// This file is part of [propulse]ART.
+//
+// [propulse]ART is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// [propulse]ART is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with [propulse]ART.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+/** \file
+ *      Just the License GPL 3+
+ *
+ *      Detailed description here.
+ *      Continues here.
+ *      And more.
+ *      And more.
+ */
+#include "config.h"
+#include "optionArgs.h"
+
+int main (int argc, char **argv)
+{
+	int result;
+	OptionArgs options;
+
+	options.add("try", 't', &result,"try it out", "arg desc") ;
+	options.doit(argc,argv);
+	return 0;
+}
+
+
+
