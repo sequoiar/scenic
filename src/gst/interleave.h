0b09f12e96f25a79b1de9f9cac08cd58ba74cb8e
Tristan Matthews <tristan@sat.qc.ca>
2008-07-14 18:45:43 +0000

    pulled out interleave code

---
+++ src/gst/interleave.h
@@ -0,0 +1,44 @@
+// interleave.h
+// Copyright 2008 Koya Charles & Tristan Matthews
+//
+// This file is part of [propulse]ART.
+//
+// [propulse]ART is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// [propulse]ART is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with [propulse]ART.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#ifndef _INTERLEAVE_H_
+#define _INTERLEAVE_H_
+
+#include "gstBase.h"
+#include <gst/audio/multichannel.h>
+
+class AudioConfig;
+
+class Interleave : public GstBase
+{
+public:
+    Interleave(const AudioConfig &config);
+    ~Interleave();
+    void init();
+    void linkInput(GstElement *element);
+    void linkOutput(GstElement *element);
+private:
+    GstElement *interleave_;
+    const AudioConfig &config_;
+    static const GstAudioChannelPosition VORBIS_CHANNEL_POSITIONS[][8];
+    void set_channel_layout();
+};
+
+#endif //_INTERLEAVE_H_
+
