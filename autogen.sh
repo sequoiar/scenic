1416ce210c1bfe4ab446ec58014f29b3e92de8df
Koya Charles <koya.charles@gmail.com>
2008-07-29 18:18:59 +0000

    autogen.sh reversion

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # could be replaced with autoconf -v -f (verbose, force rebuild of ltmain, .in files, etc.)
-uncrustify -c utils/uncrustify2.cfg --suffix "" `find . -name "*.cpp"`
-uncrustify -c utils/uncrustify2.cfg --suffix "" `find . -name "*.h"`
+#uncrustify -c utils/uncrustify2.cfg --suffix "" `find . -name "*.cpp"`
+#uncrustify -c utils/uncrustify2.cfg --suffix "" `find . -name "*.h"`
 #exit 0
 aclocal
 libtoolize --force
