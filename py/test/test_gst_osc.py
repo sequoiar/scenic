b2d9d88319979b96985ab355c883f03eb433e853
Koya Charles <koya.charles@gmail.com>
2008-07-30 22:16:12 +0000

    python script to test c code

---
+++ py/test/test_gst_osc.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+# twisted imports
+from twisted.internet import reactor, protocol
+
+# OSC imports
+import osc_protocol
+
+
+def client():
+    """Client example"""
+    message = osc_protocol.OscMessage()
+    message.setAddress('/gst')
+    message.append('init')
+    message.append(34)
+    c.send_message('127.0.0.1', 7770, message)
+    reactor.callLater(1, client2)
+
+
+def client2():
+    """Client example"""
+    message = osc_protocol.OscMessage()
+    message.setAddress('/gst')
+    message.append('start')
+    message.append(334)
+    c.send_message('127.0.0.1', 7770, message)
+    reactor.callLater(1, client3)
+
+def cquit():
+    """Client example"""
+    message = osc_protocol.OscMessage()
+    message.setAddress('/quit')
+    c.send_message('127.0.0.1', 7770, message)
+    reactor.stop()
+
+def client3():
+    """Client example"""
+    message = osc_protocol.OscMessage()
+    message.setAddress('/gst')
+    message.append('stop')
+    message.append(334)
+    c.send_message('127.0.0.1', 7770, message)
+    reactor.callLater(1, client2)
+    reactor.callLater(10, cquit);
+
+if __name__ == "__main__":
+    # Client example
+    c = osc_protocol.Osc()
+    t = reactor.listenUDP(7771, c)
+    print t.getHost()
+    reactor.callLater(1, client)
+    reactor.run()
