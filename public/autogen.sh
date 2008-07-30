6ad6f3db0fef71c225333198aa62e11b66fe3d9b
Koya Charles <koya.charles@gmail.com>
2008-07-30 19:07:04 +0000

    made a uncrustify.sh removed from autogen.sh

--- public/autogen.sh
+++ public/autogen.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
 # could be replaced with autoconf -v -f (verbose, force rebuild of ltmain, .in files, etc.)
-#uncrustify -c utils/uncrustify2.cfg --no-backup `find src/ -name "*.cpp"`
-#uncrustify -c utils/uncrustify2.cfg --no-backup `find src/ -name "*.h"`
-#exit 0
 aclocal
 libtoolize --force
 autoconf -f
 automake -a
 ./configure 'CXXFLAGS=-O0 -g'
-#./configure
