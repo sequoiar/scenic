f5ce7e17db5475be92b2bc5b611817183a82642f
Koya Charles <koya.charles@gmail.com>
2008-08-04 19:18:36 +0000

    refactor osc

--- src/osc/osc.h
+++ src/osc/osc.h
@@ -1,21 +1,21 @@
 /* headerGPL.c
-   // Copyright 2008 Koya Charles & Tristan Matthews
-   //
-   // This file is part of [propulse]ART.
-   //
-   // [propulse]ART is free software: you can redistribute it and/or modify
-   // it under the terms of the GNU General Public License as published by
-   // the Free Software Foundation, either version 3 of the License, or
-   // (at your option) any later version.
-   //
-   // [propulse]ART is distributed in the hope that it will be useful,
-   // but WITHOUT ANY WARRANTY; without even the implied warranty of
-   // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   // GNU General Public License for more details.
-   //
-   // You should have received a copy of the GNU General Public License
-   // along with [propulse]ART.  If not, see <http://www.gnu.org/licenses/>.
- */
+// Copyright 2008 Koya Charles & Tristan Matthews
+//
+// This file is part of [propulse]ART.
+//
+// [propulse]ART is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// [propulse]ART is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with [propulse]ART.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 /** \file
  *      Osc Message and Thread
@@ -29,83 +29,12 @@
 #ifndef __OSC_THREAD_H__
 #define __OSC_THREAD_H__
 
-#include "gutil/baseThread.h"
 #include <lo/lo.h>
 #include <string>
 #include <vector>
 #include <iostream>
-
-class OscMessage
-{
-    public:
-        OscMessage(const char *p, const char *t, lo_arg ** v, int c, void *d);
-        OscMessage()
-            : path_(), types_(), args_(), argc_(0), data_(0){}
-
-        OscMessage(const OscMessage& in); 
-        OscMessage& operator=(const OscMessage& in); 
-
-        const char* path() { return path_.c_str(); }
-        bool pathIsSet() { return !path_.empty(); }
-        bool pathEquals(const char *str) { return path_ == str; }
-
-        lo_message *init_msg(lo_message *msg);
-        void print();
-
-        bool argEquals(const char *str, int idx) { return args_[idx].equals(str); }
-
-    private:
-        class LoArg
-        {
-            public:
-                LoArg(const char *pchar, int index, lo_arg * a)
-                    : type_(static_cast<lo_type>(pchar[index])), i_(0), s_()
-                {
-                    switch ((char) type_)
-                    {
-                        case 's':
-                            {
-                                s_ = static_cast < char *>(&(a->s));
-                                break;
-                            }
-                        case 'i':
-                            {
-                                i_ = static_cast < int >(a->i);
-                                break;
-                            }
-                    }
-                }
-
-                void print() const;
-
-                bool equals(const char *str)
-                {
-                    if ((char) type_ == 's')
-                        return s_ == str;
-
-                    return false;
-                }
-                
-                bool equals(int val)
-                {
-                    if ((char) type_ == 'i')
-                        return i_ == val;
-
-                    return false;
-                }
-
-
-                lo_type type_;
-                int i_;
-                std::string s_;
-        };
-
-        typedef std::vector < LoArg > OscArgs;
-        std::string path_, types_;
-        OscArgs args_;
-        int argc_;
-        void *data_;
-};
+#include "gutil/baseThread.h"
+#include "oscMessage.h"
 
 
 typedef QueuePair_ < OscMessage > OscQueue;
---
+++ src/osc/oscMessage.h
@@ -0,0 +1,82 @@
+
+/* headerGPL.c
+// Copyright 2008 Koya Charles & Tristan Matthews
+//
+// This file is part of [propulse]ART.
+//
+// [propulse]ART is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// [propulse]ART is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with [propulse]ART.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/** \file
+ *      Osc Message and Thread
+ *
+ *      Detailed description here.
+ *      Continues here.
+ *      And more.
+ *      And more.
+ */
+
+#ifndef __OSC_MESSAGE_H__
+#define __OSC_MESSAGE_H__
+
+#include "gutil/baseThread.h"
+#include <lo/lo.h>
+#include <string>
+#include <vector>
+#include <iostream>
+
+class OscMessage
+{
+    public:
+        OscMessage(const char *p, const char *t, lo_arg ** v, int c, void *d);
+        OscMessage()
+            : path_(), types_(), args_(), argc_(0), data_(0){}
+
+        OscMessage(const OscMessage& in); 
+        OscMessage& operator=(const OscMessage& in); 
+
+        const char* path() { return path_.c_str(); }
+        bool pathIsSet() { return !path_.empty(); }
+        bool pathEquals(const char *str) { return path_ == str; }
+
+        lo_message *init_msg(lo_message *msg);
+        void print();
+
+        bool argEquals(std::string str, int idx) { return args_[idx].equals(str); }
+
+    private:
+        class LoArg
+        {
+            public:
+                LoArg(const char *pchar, int index, lo_arg * a);
+
+                void print() const;
+
+                bool equals(std::string str);
+
+                bool equals(int val);
+
+                lo_type type_;
+                int i_;
+                std::string s_;
+        };
+
+        typedef std::vector < LoArg > OscArgs;
+        std::string path_, types_;
+        OscArgs args_;
+        int argc_;
+        void *data_;
+};
+#endif
+
