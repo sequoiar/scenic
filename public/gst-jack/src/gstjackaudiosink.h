e8e55f28f4778eb6d038d9b150349caf831ec005
Tristan Matthews <tristan@sat.qc.ca>
2008-08-06 19:22:21 +0000

    gst-jack changes

--- public/gst-jack/src/gstjackaudiosink.h
+++ public/gst-jack/src/gstjackaudiosink.h
@@ -1,7 +1,7 @@
 /* GStreamer
  * Copyright (C) 2006 Wim Taymans <wim@fluendo.com>
  *
- * gstjacksink.h:
+ * gstjackaudiosink.h:
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
