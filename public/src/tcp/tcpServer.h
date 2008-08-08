e5fbfb2607704e0196e7d61814e001961d1c92d1
Koya Charles <koya.charles@gmail.com>
2008-08-08 15:27:37 +0000

    tcpServer implementation compiles

--- public/src/tcp/tcpServer.h
+++ public/src/tcp/tcpServer.h
@@ -1,27 +1,25 @@
 #ifndef TCP_SERVER
 #define TCP_SERVER
 
+#define BUFFSIZE     256
+
 class TcpServer
 {
     public:
-        TcpServer(int port)
-            : port_(port),connected_(false){}
-
-        bool send(const std::string& msg){ return !msg.empty();}
-        bool socket_bind_listen(){ return true;}
-        bool accept(){ return true;}
-        bool connected(){return connected_;}
-        bool read(std::string& buff){
-            if(!connected_)
-                return false;
-            buff = "data";
-            return true;
-        }
-
-
+    TcpServer(int port): sockfd(0), newsockfd(0), buffer_(), port_(port) ,connected_(false){}
+    bool recv(std::string& out);
+    bool send(const std::string& in);
+    bool socket_bind_listen();
+    bool accept();
+    bool close();
+    bool connected(){return connected_;}
     private:
-		int port_;
-        bool connected_;
+    int sockfd,newsockfd;
+
+    char buffer_[BUFFSIZE];
+    int port_;
+    bool connected_;
 };
+
 
 #endif
