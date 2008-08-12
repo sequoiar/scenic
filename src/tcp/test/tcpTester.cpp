7ff7a81822f92f15490d518933e3511b9da8dcfb
Koya Charles <koya.charles@gmail.com>
2008-08-12 18:04:39 +0000

    tcp tester added to svn

---
+++ src/tcp/test/tcpTester.cpp
@@ -0,0 +1,33 @@
+#include "tcp/tcpThread.h"
+#include <cassert>
+#include <iostream>
+int main(int argc, char** argv)
+{
+    if(argc < 2)
+        return 0;
+    int port = atoi(argv[1]);
+    TcpThread tcp(port);
+    TcpQueue& queue = tcp.getQueue();
+
+    tcp.run();
+
+    while(1)
+    {
+        StdMsg f = queue.timed_pop(1000000);
+        if(f.get_type() == StdMsg::STD)
+        {
+            std::cout << f.getMsg() << std::endl;
+            if(!f.getMsg().compare(0, 4, "quit"))
+            {
+                StdMsg q(StdMsg::QUIT);
+                queue.push(q);
+                break;
+            }
+            else{
+                tcp.send(f.getMsg());
+            }
+        }
+    }
+}
+
+
