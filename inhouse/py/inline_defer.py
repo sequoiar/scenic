180835241b5e84c5782635429f0c334d922530d0
Étienne Désautels <etienne@teknozen.net>
2008-08-14 15:33:31 +0000

    Added own class and function that return a defer (instead of getPage).

--- inhouse/py/inline_defer.py
+++ inhouse/py/inline_defer.py
@@ -5,13 +5,30 @@ from twisted.web.client import getPage
 import time
 
 
+class ComTest(object):
+    def __init__(self):
+        self.deferred = defer.Deferred()
+
+    def send(self, cmd):
+        reactor.callLater(2, self.receive, cmd)
+        return self.deferred
+    
+    def receive(self, cmd):
+        self.deferred.callback(cmd*2)
+
+
+def get_data(cmd):
+    com = ComTest()
+    return com.send(cmd)
+
+
 @defer.inlineCallbacks
 def test():
     print 'Start test'
     query = "http://www.google.com"
     prev = time.time()
     print 'before getPage', prev
-    content = yield getPage(query)
+    content = yield get_data(query)
 #    print content
     print 'after getPage', time.time() - prev
 #    return content
@@ -28,8 +45,8 @@ def main():
     print "Starting ", prev
     t = test()
     print "Stoping ", time.time() - prev
-#    t.addCallback(out)
-    reactor.callLater(5, out, t)
+    t.addCallback(out)
+#    reactor.callLater(5, out, t)
 #    print t
 #    reactor.stop()
 
