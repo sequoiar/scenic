9467d7f8203030d0ba0c51ff5bde27e0e44df70a
Koya Charles <koya.charles@gmail.com>
2008-08-18 18:19:37 +0000

    removed prototypes from build - removed config.h.in - added autoheader to autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -3,6 +3,7 @@
 # could be replaced with autoconf -v -f (verbose, force rebuild of ltmain, .in files, etc.)
 aclocal
 libtoolize --force
+autoheader
 autoconf -f
 automake -a
 ./configure 'CXXFLAGS=-O0 -g'
