8c8b6fd972f95f9bcce16a23c189c1255b13b055
Tristan Matthews <tristan@sat.qc.ca>
2008-08-29 21:47:56 +0000

    commented out unused code

--- src/gst/gstBase.h
+++ src/gst/gstBase.h
@@ -37,12 +37,10 @@ class GstBase
         GstBase();
         virtual ~GstBase();
 
-//        static bool link_pads(GstPad *srcPad, GstPad *sinkPad);
-        
         Pipeline & pipeline_;
 
     private:
-        static int refCount_;
+        //static int refCount_;
 };
 
 #endif // _GST_BASE_H_
