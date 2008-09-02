1e7b37c7452da7074457b99b234d312908f19548
Koya Charles <koya.charles@gmail.com>
2008-09-02 19:10:29 +0000

    parser testing

--- public/src/tcp/parser.cpp
+++ public/src/tcp/parser.cpp
@@ -1,5 +1,8 @@
 #include "parser.h"
 #include "logWriter.h"
+
+
+
 std::string strEsq(std::string& str)
 {
     std::string out;
@@ -55,5 +58,61 @@ int get_end_of_quoted_string(std::string& str)
     LOG_WARNING("String has no terminating \".");
     return 0; 
 }
+
+bool tokenize(std::string& str, std::map<std::string,std::string> &cmd_map) 
+{
+    unsigned int i;
+    const char *cstr = str.c_str();
+    std::string lstr = str;
+    cmd_map.clear();
+    i = strcspn(cstr,":");
+    if(i == str.size())
+        return false;
+  
+    LOG_DEBUG(lstr.substr(0,i));
+    cmd_map.insert( make_pair( "command", lstr.substr(0,i)) );
+
+    lstr = lstr.substr(i+1);
+    for(;;)
+    {
+        cstr = lstr.c_str();
+        i = strcspn(cstr,"=");
+        if(i == lstr.size())
+            break;
+    
+        if(cstr[i+2] == '\"')
+        {
+            std::string tstr = lstr.substr(i+2);
+            int pos = get_end_of_quoted_string(tstr);
+            if(pos == 0)
+                return false;
+
+            std::string quote = lstr.substr(i+2,pos);
+            LOG_DEBUG(quote);
+
+            cmd_map.insert( make_pair(lstr.substr(0,i),quote));
+        }
+        else
+        {
+            int pos = strcspn(cstr+i+2," ");
+            
+            LOG_DEBUG(lstr.substr(0,i));
+
+
+            LOG_DEBUG(lstr.substr(i+2,pos));
+
+
+            cmd_map.insert( make_pair(lstr.substr(0,i),lstr.substr(i+2,pos)));
+
+
+
+
+        }
+        
+
+    }
+    return true;
+}
+
 
 
