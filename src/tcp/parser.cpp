9026581efb795f634930897b80805f03f2592a8c
Koya Charles <koya.charles@gmail.com>
2008-09-08 02:03:25 +0000

    comments

--- src/tcp/parser.cpp
+++ src/tcp/parser.cpp
@@ -16,6 +16,8 @@
  * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.
  */
+
+
 #include <sstream>
 #include "parser.h"
 #include "logWriter.h"
@@ -26,18 +28,18 @@ std::string strEsq(const std::string& str)
 {
     std::string out;
 
-    for(unsigned int pos=0; pos < str.size(); ++pos) //for each char in string
+    for(unsigned int pos=0; pos < str.size(); ++pos)    //for each char in string
     {
-        char c = str[pos];              //copy current character
-        if(c == '\\')                 //if backslash found
-            out.append("\\\\");      //escape it with backslash
-        else if(c == '\"')            //if quotation mark
-            out.append("\\\"");      //escape it with backslash
+        char c = str[pos];                              //copy current character
+        if(c == '\\')                                   //if backslash found
+            out.append("\\\\");                         //escape it with backslash
+        else if(c == '\"')                              //if quotation mark
+            out.append("\\\"");                         //escape it with backslash
         else
-            out.append(1, c);           //otherwise pass it through
+            out.append(1, c);                           //otherwise pass it through
     }
 
-    return out;                         //return copy char to output
+    return out;                                         //return copy char to output
 }
 
 
@@ -48,18 +50,18 @@ std::string strUnEsq(const std::string& str)
     //for each char in string
     for(unsigned int pos=0; pos < str.size(); ++pos)
     {
-        char c = str[pos];              //copy first character
-        char c2 = str[pos+1];           //copy second character
-        if(c == '\\')                   //if first character is backslash
+        char c = str[pos];                              //copy first character
+        char c2 = str[pos+1];                           //copy second character
+        if(c == '\\')                                   //if first character is backslash
         {
-            if(c2 == '\\')              //and second character is
-            {                           //also backslash
-                out.append("\\");       //add one backslash
-                ++pos;                  //increment pos
+            if(c2 == '\\')                              //and second character is
+            {                                           //also backslash
+                out.append("\\");                       //add one backslash
+                ++pos;                                  //increment pos
             }
         }
         else
-            out.append(1, c);           //otherwise copy char to output
+            out.append(1, c);                           //otherwise copy char to output
     }
 
     return out;
@@ -76,9 +78,9 @@ int get_end_of_quoted_string(const std::string& str)
     //for each char in string
     for(unsigned int pos=1; pos < str.size(); ++pos)
     {
-        if(str[pos] == '\"')          //if char is " and if
-            if(str[pos-1] != '\\')    //previous char is not escape char
-                return pos+1;           //return position following "
+        if(str[pos] == '\"')                            //if char is " and if
+            if(str[pos-1] != '\\')                      //previous char is not escape char
+                return pos+1;                           //return position following "
     }
 
     LOG_WARNING("String has no terminating \".");
@@ -91,7 +93,7 @@ bool stringify(std::map<std::string, StrIntFloat>& cmd_map, std::string& str)
     typedef std::map<std::string, StrIntFloat>::iterator iter;
     str.clear();
 
-    //locate "command" and output it to str
+    //locate "command" and output value to str
     iter it = cmd_map.find(std::string("command"));
     if(it != cmd_map.end())
     {
@@ -109,26 +111,26 @@ bool stringify(std::map<std::string, StrIntFloat>& cmd_map, std::string& str)
     for(it = cmd_map.begin();
         it != cmd_map.end(); ++it)
     {
-        if(it->first.compare("command"))        //Insure it's not the command
+        if(it->first.compare("command"))                //Insure it's not the command
         {
             str.append(" ");
-            str.append(it->first);              //Output the key to the str
+            str.append(it->first);                      //Output the key to the str
             str.append("=");
-            switch(it->second.type())           //Output the value to the str
+            switch(it->second.type())                   //Output the value to the str
             {
-                case 's':                       //If the value is a string
-                {                               //delimit with "
+                case 's':                               //If the value is a string
+                {                                       //delimit with "
                     std::string temp;
                     if(!it->second.get(temp))
                         return false;
                     str.append("\"");
-                    temp = strEsq(temp);        //Escape the string contents
+                    temp = strEsq(temp);                //Escape the string contents
                     str.append(temp);
                     str.append("\"");
                     break;
                 }
-                case 'i':                       //If the value is integer
-                {                               //generate string from int
+                case 'i':                               //If the value is integer
+                {                                       //generate string from int
                     int temp;
                     if(!it->second.get(temp))
                         return false;
@@ -137,8 +139,8 @@ bool stringify(std::map<std::string, StrIntFloat>& cmd_map, std::string& str)
                     str.append(sstream.str());
                     break;
                 }
-                case 'f':                       //If the value is a float
-                {                               //generate string from float
+                case 'f':                               //If the value is a float
+                {                                       //generate string from float
                     float temp;
                     if(!it->second.get(temp))
                         return false;
@@ -153,8 +155,8 @@ bool stringify(std::map<std::string, StrIntFloat>& cmd_map, std::string& str)
         }
     }
 
-    str.append("\r\\\n");                       //Append \r\n to conform to
-    //telnet specification
+    str.append("\r\\\n");                               //Append \r\n to conform to
+                                                        //telnet specification
 
     return true;
 }
@@ -164,66 +166,66 @@ bool tokenize(const std::string& str, std::map<std::string, StrIntFloat> &cmd_ma
 {
     unsigned int i;
     const char *cstr = str.c_str();
-    std::string lstr = str;                             //copy in str to local string
-    cmd_map.clear();                                    //clear output map
+    std::string lstr = str;                                 //copy in str to local string
+    cmd_map.clear();                                        //clear output map
 
-    i = strcspn(cstr, ":");                              //search for ":"
-    if(i == str.size())                                 //if : not found return error
+    i = strcspn(cstr, ":");                                 //search for ":"
+    if(i == str.size())                                     //if : not found return error
         return false;
     LOG_DEBUG(lstr.substr(0, i));
-    StrIntFloat c(lstr.substr(0, i));                    //make a string for command
-    cmd_map.insert( std::make_pair( "command", c) );    //insert command into map
+    StrIntFloat c(lstr.substr(0, i));                       //make a string for command
+    cmd_map.insert( std::make_pair( "command", c) );        //insert command into map
 
 
-    lstr = lstr.substr(i+2);                            //set lstring beyond command
+    lstr = lstr.substr(i+2);                                //set lstring beyond command
 
     //loop until break or return
     for(;;)
     {
         int pos;
         cstr = lstr.c_str();
-        i = strcspn(cstr, "=");                          //search of "="
-        if(i == lstr.size())                            //if not found no more
-            break;                                      //key=value pairs so break loop
+        i = strcspn(cstr, "=");                             //search of "="
+        if(i == lstr.size())                                //if not found no more
+            break;                                          //key=value pairs so break loop
 
-        if(cstr[i+1] == '\"')                           //if value looks like a string
+        if(cstr[i+1] == '\"')                               //if value looks like a string
         {
             std::string tstr = lstr.substr(i+1);
-            pos = get_end_of_quoted_string(tstr);       //find end of "
-            if(pos == 0)                                //if value not string
-                return false;                           //return error
+            pos = get_end_of_quoted_string(tstr);           //find end of "
+            if(pos == 0)                                    //if value not string
+                return false;                               //return error
 
-            tstr = lstr.substr(0, i);                    //tstr is key
+            tstr = lstr.substr(0, i);                       //tstr is key
             LOG_DEBUG(tstr);
 
 
-            std::string quote = lstr.substr(i+2, pos-2); //strip begin and end quotes
-            quote = strUnEsq(quote);                    //clean any escape backslashes
+            std::string quote = lstr.substr(i+2, pos-2);    //strip begin and end quotes
+            quote = strUnEsq(quote);                        //clean any escape backslashes
             LOG_DEBUG(quote);
-            StrIntFloat q(quote);                       //make string for value
-            cmd_map.insert( make_pair(tstr, q));         //insert key,value into map
+            StrIntFloat q(quote);                           //make string for value
+            cmd_map.insert( make_pair(tstr, q));            //insert key,value into map
         }
         else{
-            pos = strcspn(cstr+i+1, " ");                //find end of key=value pair
+            pos = strcspn(cstr+i+1, " ");                   //find end of key=value pair
 
-            LOG_DEBUG(lstr.substr(0, i));
-            LOG_DEBUG(lstr.substr(i+1, pos));
-
-            std::stringstream stream(lstr.substr(i+1, pos)); //string of value
-            if(strcspn(stream.str().c_str(), ".")            //if value does not
+            std::stringstream stream(lstr.substr(i+1, pos));//string of value
+            if(strcspn(stream.str().c_str(), ".")           //if value does not
                == stream.str().size())                      //contain . it is an int
             {
                 int temp_i;
                 stream >> temp_i;                           //convert str to int
                 StrIntFloat temp(temp_i);                   //make int
-                cmd_map.insert( make_pair(lstr.substr(0, i), temp)); //insert key,value
+                cmd_map.insert(                             //insert key,valu
+                    make_pair(lstr.substr(0, i), temp)); 
             }
-            else{                                           //value contains .
+            else
+            {                                               //value contains .
                                                             //thus it is a float
                 float temp_f;
                 stream >> temp_f;                           //convert str to float
                 StrIntFloat temp(temp_f);                   //make float
-                cmd_map.insert( make_pair(lstr.substr(0, i), temp)); //insert key,value
+                cmd_map.insert(                             //insert key,value
+                    make_pair(lstr.substr(0, i), temp)); 
             }
         }
         if(lstr.size() > i+2+pos)                           //more characters to process
