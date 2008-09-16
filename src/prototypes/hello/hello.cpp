46c3a109a272826704578505c2b36779b27c6955
Koya Charles <koya.charles@gmail.com>
2008-09-16 22:06:09 +0000

    python c/threads example

--- src/prototypes/hello/hello.cpp
+++ src/prototypes/hello/hello.cpp
@@ -1,3 +1,8 @@
+#include <glib.h>
+#include <cstring>
+#include <cstdlib>
+#include "Python.h"
+
 #include "hello.h"
 
 const char *Hello::greet()
@@ -9,10 +14,35 @@ const char *Hello::greet()
     return ts.c_str();
 }
 
+int i = 0;
+gpointer gMain(gpointer )
+{
+    while(1)
+    {
+        usleep(1000000);
+        {
+            PyGILState_STATE state = PyGILState_Ensure();
+            char t[100];
+            sprintf(t,"print %d",i);
+            PyRun_SimpleString(t);
+
+            PyGILState_Release(state);
+
+
+        }
+    }
+
+}
+
 
 void Hello::set_name(char const *n)
 {
     s = n;
+    g_thread_init(0);
+
+    PyEval_InitThreads();
+    i = atoi(n);
+    g_thread_create(gMain,0,0,0);
 }
 
 
