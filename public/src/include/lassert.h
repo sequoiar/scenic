511f9bec61879671804e20c189e6c63dfe44aa59
Koya Charles <koya.charles@gmail.com>
2008-09-16 19:00:07 +0000

    removed extern

--- public/src/include/lassert.h
+++ public/src/include/lassert.h
@@ -45,7 +45,7 @@
      :  assert_throw(__STRING(expr), __FILE__, __LINE__, __ASSERT_FUNCTION))
 
 
-extern void assert_throw(__const char *__assertion, __const char *__file,
+void assert_throw(__const char *__assertion, __const char *__file,
                            unsigned int __line, __const char *__function);
 
 #endif  //ASSERT_THROWS
