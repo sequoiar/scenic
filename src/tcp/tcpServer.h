46612542af2fec4215700e9a69d9d0ca5ac5d5ef
Koya Charles <koya.charles@gmail.com>
2008-09-18 18:05:53 +0000

    comments and GPL license

--- src/tcp/tcpServer.h
+++ src/tcp/tcpServer.h
@@ -1,3 +1,28 @@
+// tcpServer.h
+// Copyright 2008 Koya Charles & Tristan Matthews 
+//     
+// This file is part of [propulse]ART.
+//
+// [propulse]ART is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// [propulse]ART is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with [propulse]ART.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+/** \file 
+ *      TcpServer class  
+ *
+ */
+
+
 #ifndef TCP_SERVER
 #define TCP_SERVER
 
@@ -25,6 +50,7 @@ class TcpServer
         char buffer_[BUFFSIZE];
         int port_;
         bool connected_;
+     
 };
 
 
