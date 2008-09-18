79d2400e120220d6d5d292233a4989d87d317aca
Koya Charles <koya.charles@gmail.com>
2008-09-18 22:06:53 +0000

    tests

--- public/src/tcp/test/parserTester.cpp
+++ public/src/tcp/test/parserTester.cpp
@@ -6,25 +6,56 @@
 #include "logWriter.h"
 #include "mapMsg.h"
 
-std::string str = "init: it=103945 mystring=\"\\\"bar you\\\"\" Hellotab=1.1";
+#include <cpptest.h>
 
-
-int main(int, char**)
+class ParserTester : public Test::Suite
 {
+    public:
+        ParserTester()
+        {
+            TEST_ADD(ParserTester::tokenize_test)
+            TEST_ADD(ParserTester::stringify_test)
+            TEST_ADD(ParserTester::roundtrip_test)
+
+        }
+
+private:
+        void tokenize_test();
+        void stringify_test();
+        void roundtrip_test();
+
+};
+
+void ParserTester::tokenize_test()
+{ 
+    TEST_ASSERT(1);
+    LOG_ERROR("Here");
+    TEST_ASSERT(1);
+}
+
+void ParserTester::stringify_test(){}
+
+void ParserTester::roundtrip_test()
+{
+    std::string str = "init: it=103945 mystring=\"\\\"bar you\\\"\" Hellotab=1.1";
     std::map<std::string, StrIntFloat> mymap;
     if(tokenize(str, mymap))
         LOG_DEBUG("Success");
         else
-            LOG_DEBUG("Fail");
+    LOG_DEBUG("Fail");
 
             std::string out;
     if(stringify(mymap, out))
         LOG_DEBUG(out);
-        else
-            LOG_DEBUG("Fail stringify");
+    else
+        LOG_DEBUG("Fail stringify");
 
 
-            return 0;
 }
 
-
+int main(int, char**)
+{
+    ParserTester pt;
+    Test::TextOutput output(Test::TextOutput::Verbose);
+    return pt.run(output,false);
+}
