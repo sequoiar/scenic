114d35172fb5379906a64e6584ffd31e694e14fe
Koya Charles <koya.charles@gmail.com>
2008-09-25 22:29:53 +0000

    Get caps from Msg send to AudioReceiver

--- public/src/main/main.cpp
+++ public/src/main/main.cpp
@@ -110,7 +110,7 @@ int main (int argc, char** argv)
     }
     catch(std::string err)
     {
-        std::cerr << "GOING DOWN " << err;
+        std::cerr << "Does it change? " << err;
     }
     
     try
