c1853b5ffb26b759d1b5ed05b15c802ff9769d08
Koya Charles <koya.charles@gmail.com>
2008-09-29 02:36:27 +0000

    msgThread now sends a quit msg to self in destructor so that thread exits

--- src/include/msgThread.h
+++ src/include/msgThread.h
@@ -1,3 +1,30 @@
+// headerGPL.c
+// Copyright 2008 Koya Charles & Tristan Matthews 
+//     
+// This file is part of [propulse]ART.
+//
+// [propulse]ART is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// [propulse]ART is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with [propulse]ART.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+/** \file 
+ *      Just the License GPL 3+ 
+ *
+ *      Detailed description
+ */
+#ifndef __MSG_THREAD_H__
+#define __MSG_THREAD_H__
+
 #include "baseThread.h"
 #include "mapMsg.h"
 
@@ -6,5 +33,15 @@ class MsgThread
     : public BaseThread<MapMsg>
 {
 
-
+public:
+    ~MsgThread()
+    {
+        if(th_){
+        MapMsg msg;
+        msg["command"] = StrIntFloat("quit");
+        flippedQueue_.push(msg);
+        }
+    }
 };
+
+#endif
