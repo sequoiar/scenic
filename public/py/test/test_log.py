643fd4b7f4c54cf2db16980eaf791fb93d9feaab
Antoine Collet <antoine.collet@gmail.com>
2008-09-29 15:44:28 +0000

    unit test for log.py

---
+++ public/py/test/test_log.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Sropulpof
+# Copyright (C) 2008 Soci�t� des arts technoligiques (SAT)
+# http://www.sat.qc.ca
+# All rights reserved.
+#
+# This file is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Sropulpof is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Sropulpof.  If not, see <http:#www.gnu.org/licenses/>.
+
+import os
+import sys
+from twisted.trial import unittest
+
+#import address_book
+d = os.path.abspath('../utils/')
+sys.path.append(d)
+__import__('log')
+
+import log
+#test to_utf and open
+
+def generateString(sauf = ''):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz_@12345-6789.0!?=+%$()[]{}#<>€£éèêëà§çπ‡Ò∂ƒﬁ~'
+    pbchar = '/"\'`¬µ‹≈©◊≠÷'
+    problem_with_those_char = '&â†°'
+    string=''
+    for x in range(len(alphabet)):
+        if ( not alphabet[x] in sauf):
+            string+=alphabet[x]
+    return string
+
+class TestLog(unittest.TestCase):
+    
+    def setUp(self):
+        log.start()
+    
+    def tearDown(self):
+        log.stop()
+    
+    def test_start(self):  
+        #to test log_name     
+        levels = ['info', 'warning', 'error', 'critical', 'debug']
+        #assert ( log.start('info', 1, 1) == 'twisted'), self.fail('problem getting the correct log name')
+        res = log.start('info', 1, 1)
+        assert(res == None), self.fail('can\'t get the instance of the logger')
+               
+        for i in levels:
+            res = log.start(i, 1, 1, generateString())
+            assert (res != None), self.fail('problem getting the correct log name after creating')
+            log.stop()
+            
+        
+    def test_stop(self):
+        log.stop()
+        
+    def test_set_level(self):
+        levels = ['info', 'warning', 'error', 'critical', 'debug']
+        
+        for i in levels:
+            log.set_level(i)
+        
+    def test_critical(self):
+        log.critical(generateString())
+        
+    def test_error(self):
+        log.error(generateString())
+        
+    def test_warning(self):
+        log.warning(generateString())
+        
+    def test_info(self):
+        log.info(generateString())
+        
+        
+    def test_debug(self):
+        log.debug(generateString())                        
