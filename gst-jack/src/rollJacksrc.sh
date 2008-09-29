670de324f33304b58536587a782b4ec91afc902e
Tristan Matthews <tristan@sat.qc.ca>
2008-09-29 21:53:56 +0000

    levels prototype done, as of yet unused audioLevel added

--- gst-jack/src/rollJacksrc.sh
+++ gst-jack/src/rollJacksrc.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-LEVEL=5
+LEVEL=0
 ELEMENT="jackaudiosrc:"
-FLAGS="-m --gst-debug-level=${ELEMENT}${LEVEL}"
+FLAGS="-m --gst-debug-level=${ELEMENT}${LEVEL} -v"
 AUTOCONNECT="connect=0"
 gst-launch $FLAGS jackaudiosrc $AUTOCONNECT ! jackaudiosink $AUTOCONNECT sync=FALSE
