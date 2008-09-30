e94b93285d4a94ae8af463bf818759348a3a823c
Koya Charles <koya.charles@gmail.com>
2008-09-30 21:39:14 +0000

    StrIntFloat now has a vector of doubles (rename KitchenSink?)

--- public/src/util/test/utilTester.cpp
+++ public/src/util/test/utilTester.cpp
@@ -1,10 +1,17 @@
 #include <cassert>
 #include "logWriter.h"
+#include "mapMsg.h"
 
 int main(int, char**)
 {
-    THROW_ERROR("hello");
-    assert(1);
-    assert(0);
-
+    std::vector<double> v(10),v2;
+    v[1] = 1.12;
+    v[2] = 21.12;
+    v[3] = 31.12;
+    v[4] = 41.12;
+    MapMsg msg("try");
+    msg["vector"] = StrIntFloat(v);
+    v2 = msg["vector"];
+    assert(v == v2);
+    return 0;
 }
