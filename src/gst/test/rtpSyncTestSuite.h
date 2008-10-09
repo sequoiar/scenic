103fd655f7e7e3309ee17b642407e191c614df15
Tristan Matthews <tristan@sat.qc.ca>
2008-10-09 20:46:58 +0000

    added better jack error msgs

--- src/gst/test/rtpSyncTestSuite.h
+++ src/gst/test/rtpSyncTestSuite.h
@@ -26,7 +26,7 @@
 #include <cpptest.h>
 
 class SyncTestSuiteRtp
-    : public GstTestSuite
+: public GstTestSuite
 {
     public:
 
@@ -35,15 +35,19 @@ class SyncTestSuiteRtp
             // DANGER: if this is too short you're likely to have synchronization issues
             testLength_ = 60000;
 
-            TEST_ADD(SyncTestSuiteRtp::start_8ch_audiofile_dv)
+            TEST_ADD(SyncTestSuiteRtp::start_jack_v4l)
+                TEST_ADD(SyncTestSuiteRtp::stop_jack_v4l)
+                TEST_ADD(SyncTestSuiteRtp::start_stop_jack_v4l)
+
+                TEST_ADD(SyncTestSuiteRtp::start_8ch_audiofile_dv)
                 TEST_ADD(SyncTestSuiteRtp::stop_8ch_audiofile_dv)
                 TEST_ADD(SyncTestSuiteRtp::start_stop_8ch_audiofile_dv)
 
-                TEST_ADD(SyncTestSuiteRtp::start_audiotest_videotest);
-            TEST_ADD(SyncTestSuiteRtp::stop_audiotest_videotest);
-            TEST_ADD(SyncTestSuiteRtp::start_stop_audiotest_videotest);
+                TEST_ADD(SyncTestSuiteRtp::start_audiotest_videotest)
+                TEST_ADD(SyncTestSuiteRtp::stop_audiotest_videotest)
+                TEST_ADD(SyncTestSuiteRtp::start_stop_audiotest_videotest)
 
-            TEST_ADD(SyncTestSuiteRtp::start_dv_audio_dv_video)
+                TEST_ADD(SyncTestSuiteRtp::start_dv_audio_dv_video)
                 TEST_ADD(SyncTestSuiteRtp::stop_dv_audio_dv_video)
                 TEST_ADD(SyncTestSuiteRtp::start_stop_dv_audio_dv_video)
 
@@ -64,6 +68,11 @@ class SyncTestSuiteRtp
         void start_audiotest_videotest();
         void stop_audiotest_videotest();
         void start_stop_audiotest_videotest();
+
+        void start_jack_v4l();
+        void stop_jack_v4l();
+        void start_stop_jack_v4l();
+
 };
 
 #endif // _SYNC_TEST_SUITE_H_
