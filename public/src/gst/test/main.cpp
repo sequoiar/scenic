e2292d0504510b5e16f6c0a6e9f5dc3b888af88d
Tristan Matthews <tristan@sat.qc.ca>
2008-10-10 17:40:47 +0000

    first prototype of demo program

--- public/src/gst/test/main.cpp
+++ public/src/gst/test/main.cpp
@@ -9,26 +9,30 @@ int mainRtpVideoTestSuite(int argc, char **argv);
 int mainRtpAudioTestSuite(int argc, char **argv);
 int mainAudioTestSuite(int argc, char **argv);
 
+int mainSropulpof(int argc, char **argv);
+
 int main(int argc, char **argv)
 {
    std::string filename = argv[0];
    
    if(filename.find("syncTester") != std::string::npos)
        return mainSyncTestSuite(argc,argv);
-
-   if(filename.find("videoTester") != std::string::npos)
+   else if(filename.find("videoTester") != std::string::npos)
        return mainVideoTestSuite(argc,argv);
-
-   if(filename.find("rtpVideoTester") != std::string::npos)
+   else if(filename.find("rtpVideoTester") != std::string::npos)
        return mainRtpVideoTestSuite(argc,argv);
-
-   if(filename.find("rtpAudioTester") != std::string::npos)
+   else if(filename.find("rtpAudioTester") != std::string::npos)
        return mainRtpAudioTestSuite(argc,argv);
-
-   if(filename.find("audioTester") != std::string::npos)
+   else if(filename.find("audioTester") != std::string::npos)
        return mainAudioTestSuite(argc,argv);
-
-   if(filename.find("rtpSyncTester") != std::string::npos)
+   else if(filename.find("rtpSyncTester") != std::string::npos)
        return mainRtpSyncTestSuite(argc,argv);
+   else if (filename.find("pof") != std::string::npos)
+       return mainSropulpof(argc, argv);
+   else
+   {
+       std::cout << "Error: Symbolic link name doesn't match exectuable!" << std::endl;
+        return 1;
+   }
 }
 
--- public/src/gst/test/rtpSyncTestSuite.h
+++ public/src/gst/test/rtpSyncTestSuite.h
@@ -35,6 +35,10 @@ class SyncTestSuiteRtp
             // DANGER: if this is too short you're likely to have synchronization issues
             testLength_ = 10000;
 
+                TEST_ADD(SyncTestSuiteRtp::start_audiotest_videotest)
+                TEST_ADD(SyncTestSuiteRtp::stop_audiotest_videotest)
+                TEST_ADD(SyncTestSuiteRtp::start_stop_audiotest_videotest)
+
                 TEST_ADD(SyncTestSuiteRtp::start_8ch_audiofile_dv)
                 TEST_ADD(SyncTestSuiteRtp::stop_8ch_audiofile_dv)
                 TEST_ADD(SyncTestSuiteRtp::start_stop_8ch_audiofile_dv)
@@ -42,10 +46,6 @@ class SyncTestSuiteRtp
                 TEST_ADD(SyncTestSuiteRtp::start_jack_v4l)
                 TEST_ADD(SyncTestSuiteRtp::stop_jack_v4l)
                 TEST_ADD(SyncTestSuiteRtp::start_stop_jack_v4l)
-
-                TEST_ADD(SyncTestSuiteRtp::start_audiotest_videotest)
-                TEST_ADD(SyncTestSuiteRtp::stop_audiotest_videotest)
-                TEST_ADD(SyncTestSuiteRtp::start_stop_audiotest_videotest)
 
                 TEST_ADD(SyncTestSuiteRtp::start_dv_audio_dv_video)
                 TEST_ADD(SyncTestSuiteRtp::stop_dv_audio_dv_video)
