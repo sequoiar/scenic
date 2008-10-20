31911a1e6e0deadfb4f51e430c823cc4fea4acbd
Simon Piette <simonp@sat.qc.ca>
2008-10-20 20:51:17 +0000

    Added many svn:ignore properties so svn status is clean without messing with global-ignores

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# could be replaced with autoconf -v -f (verbose, force rebuild of ltmain, .in files, etc.)
+# could be replaced with autoreconf -fivI m4 (verbose, force rebuild of ltmain, .in files, etc.)
 aclocal -I m4
 libtoolize --force
 autoheader
