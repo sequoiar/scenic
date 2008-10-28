46e2ca132ddaca4bf0536bd50660457de82e8600
Tristan Matthews <tristan@sat.qc.ca>
2008-10-28 19:09:15 +0000

    moved gst-jack to inhouse

--- gst-jack/src/rollJacksrc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-LEVEL=0
-ELEMENT="jackaudiosrc:"
-FLAGS="-m --gst-debug-level=${ELEMENT}${LEVEL} -v"
-AUTOCONNECT="connect=0"
-gst-launch $FLAGS jackaudiosrc $AUTOCONNECT ! jackaudiosink $AUTOCONNECT sync=FALSE
