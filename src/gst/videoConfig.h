3526d2ebe0e52fff209fe916beeefd32e996dc36
Koya Charles <koya.charles@gmail.com>
2008-11-20 19:32:10 +0000

    cleanup constuctors

--- src/gst/videoConfig.h
+++ src/gst/videoConfig.h
@@ -1,28 +1,22 @@
-
-// videoConfig.h
-// Copyright 2008 Koya Charles & Tristan Matthews
-//
-// This file is part of [propulse]ART.
-//
-// [propulse]ART is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// [propulse]ART is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with [propulse]ART.  If not, see <http://www.gnu.org/licenses/>.
-//
-
-/** \file
- *      Class for video parameter objects.
+/* videoConfig.h
+ * Copyright 2008 Koya Charles & Tristan Matthews 
+ *
+ * This file is part of [propulse]ART.
+ *
+ * [propulse]ART is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * [propulse]ART is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with [propulse]ART.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
-
 #ifndef _VIDEO_CONFIG_H_
 #define _VIDEO_CONFIG_H_
 
@@ -34,14 +28,15 @@ class VideoSink;
 class VideoConfig
 {
     public:
-        VideoConfig(const std::string &source__) : source_(source__), screen_num_(0), location_("") {}
-        VideoConfig(const std::string &source__, int screen_num) : source_(source__), screen_num_(screen_num), location_("") {}
+        //* for a simple source */
+        VideoConfig(const std::string &source__, int screen_num=0) 
+            : source_(source__), screen_num_(screen_num), location_("") {}
 
-        // for sender (remote) w/ location i.e. filename or url
+        //* for source (remote) w/ location i.e. filename or url */
         VideoConfig(const std::string &source__, const std::string &location__)
             : source_(source__), screen_num_(0), location_(location__)  {}
 
-        // copy constructor
+        //* copy constructor */
         VideoConfig(const VideoConfig& m)
             : source_(m.source_), screen_num_(m.screen_num_), location_(m.location_) {}
 
@@ -58,8 +53,8 @@ class VideoConfig
         const std::string source_;
         const int screen_num_;
         const std::string location_;
-
-        VideoConfig& operator=(const VideoConfig&);     //No Assignment Operator
+        /** No Assignment Operator */
+        VideoConfig& operator=(const VideoConfig&);     
 };
 
 
