500c905d1d4f9d396883007ebc1d2da081bd9026
Koya Charles <koya.charles@gmail.com>
2008-11-29 05:46:57 +0000

    test/factories.h pushed up to gst

--- src/main/sropulpof.cpp
+++ src/main/sropulpof.cpp
@@ -1,30 +1,30 @@
-// sropulpof.cpp
-// Copyright 2008 Koya Charles & Tristan Matthews
-//
-// This file is part of [propulse]ART.
-//
-// [propulse]ART is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// [propulse]ART is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with [propulse]ART.  If not, see <http://www.gnu.org/licenses/>.
-//
+/* sropulpof.cpp
+ * Copyright 2008 Koya Charles & Tristan Matthews 
+ *
+ * This file is part of [propulse]ART.
+ *
+ * [propulse]ART is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * [propulse]ART is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with [propulse]ART.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
-#include "gst/engine/test/factories.h"
+#include "gst/factories.h"
 //#include "eventLoop.h"
 //#include "videoSink.h"
 #include <cassert>
 #include <cstdlib>
 #include "logWriter.h"
 #include "gutil/optionArgs.h"
-#include "gst/gst.h"
 #include "gst/msgThreadFactory.h"
 #define BLOCK() ;
 namespace pof 
