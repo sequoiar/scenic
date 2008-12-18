eff2d04b229a7dc83581885b21d04d3ebe328521
Koya Charles <koya.charles@gmail.com>
2008-12-18 16:22:04 +0000

    fix build

--- src/python/pyMsgThread.cpp
+++ src/python/pyMsgThread.cpp
@@ -20,6 +20,35 @@
 
 #include "pyMsgThread.h"
 
+boost::python::dict makeDict(MapMsg& m)
+{ 
+    boost::python::dict d;
+    const std::pair<const std::string, StrIntFloat>* it;
+    for(it = m.begin(); it != 0; it = m.next())
+    {
+        LOG_DEBUG("");
+        switch(it->second.get_type())
+        {
+            case 's':
+                d.setdefault(it->first.c_str(),std::string(it->second));
+                break;
+            case 'i':
+                d.setdefault(it->first.c_str(),int(it->second));
+                break;
+            case 'f':
+                d.setdefault(it->first.c_str(),double(it->second));
+                break;
+            case 'F':
+                break;
+            case 'e':
+                break;
+//                default:
+//                    THROW_ERROR("Command " << it->first
+//                                         << " has unknown type " << it->second.get_type());
+        }
+    }
+    return d;
+}
 int PythonThread::main()
 {
     LOG_INFO("Python Thread");
