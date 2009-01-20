cbc9dcd0677481be24c45fcc867447748a29ea6a
Tristan Matthews <tristan@sat.qc.ca>
2009-01-20 23:07:24 +0000

    sropulpof almost ported to python

--- src/gst/engine/py/sropulpof.py
+++ src/gst/engine/py/sropulpof.py
@@ -22,36 +22,74 @@
 
 from optparse import OptionParser
 
-import libmilhouse
+from libmilhouse import *
 
-versionNum=str(libmilhouse.PACKAGE_VERSION) + '\b' + str(libmilhouse.RELEASE_CANDIDATE)
+class PofExcept(Exception): pass
 
-parser = OptionParser(version="%prog " + versionNum)
-parser.add_option("-i", "--address", 
-        dest="ip", default="127.0.0.1", help="provide ip address")
-parser.add_option("-v", "--videocodec", 
-        dest="videocodec", default="h264", help="video codec: h264, h263 or mpeg4")
-parser.add_option("-a", "--audiocodec", 
-        dest="audiocodec", default="raw", help="audio codec: vorbis, raw or mp3")
-parser.add_option("-k", "--videosink", 
-        dest="videosink", default="xvimagesink", help="videosink: xvimagesink or glimagesink")
-parser.add_option("-t", "--audioport", 
-        type="int", dest="audioport", help="audioport: 1024-60000")
-parser.add_option("-p", "--videoport", 
-        type="int", dest="videoport", help="videoport: 1024-60000")
-parser.add_option("-s", "--sender", 
-        action="store_true", dest="isSender", help="designate this process as sender")
-parser.add_option("-r", "--receiver", 
-        action="store_false", dest="isSender", help="designate this process as receiver")
-parser.add_option("-f", "--fullscreen", 
-        action="store_true", dest="fullscreen", help="set videowindow to fullsceen (receiver side only)")
-parser.add_option("-d", "--videodevice", 
-        dest="videoDevice", help="video4linux device to use: /dev/video0, /dev/video1")
-parser.add_option("-n", "--screen", 
-        type="int", dest="screenNum", help="xinerama screen number")
-parser.add_option("-c", "--numChannels", 
-        type="int", dest="numChannels", default=2, help="number of channels (sender side only)")
+def getArgs():
+    """ Parse command line arguments """
 
-(options, args) = parser.parse_args()
+    versionNum=str(PACKAGE_VERSION) + '\b' + str(RELEASE_CANDIDATE)
 
+    parser = OptionParser(version="%prog " + versionNum)
+
+    parser.add_option("-i", "--address", 
+            dest="ip", default="127.0.0.1", help="provide ip address")
+    parser.add_option("-v", "--videocodec", 
+            dest="videocodec", default="h264", help="video codec: h264, h263 or mpeg4")
+    parser.add_option("-a", "--audiocodec", 
+            dest="audiocodec", default="raw", help="audio codec: vorbis, raw or mp3")
+    parser.add_option("-k", "--videosink", 
+            dest="videosink", default="xvimagesink", help="videosink: xvimagesink or glimagesink")
+    parser.add_option("-t", "--audioport", 
+            type="int", dest="audioport", help="audioport: 1024-60000")
+    parser.add_option("-p", "--videoport", 
+            type="int", dest="videoport", help="videoport: 1024-60000")
+    parser.add_option("-s", "--sender", 
+            action="store_true", dest="isSender", help="designate this process as sender")
+    parser.add_option("-r", "--receiver", 
+            action="store_false", dest="isSender", help="designate this process as receiver")
+    parser.add_option("-f", "--fullscreen", 
+            action="store_true", dest="fullscreen", help="set videowindow to fullsceen (receiver side only)")
+    parser.add_option("-d", "--videodevice", 
+            dest="videoDevice", help="video4linux device to use: /dev/video0, /dev/video1")
+    parser.add_option("-n", "--screen", 
+            type="int", dest="screenNum", help="xinerama screen number")
+    parser.add_option("-c", "--numChannels", 
+            type="int", dest="numChannels", default=2, help="number of channels (sender side only)")
+
+    return parser.parse_args()
+
+
+def runAsSender(options):
+    """ Sends media to a remote receiver """
+    vConfig = None
+    if options.videoDevice is None:
+        vConfig = VideoSourceConfig("v4l2src")
+    else
+        vConfig = VideoSourceConfig("v4l2src", options.videoDevice)
+    
+    vTx = buildVideoSender(vConfig, options.ip, options.videoCodec, options.videoPort)
+
+    aConfig = AudioSourceConfig("jackaudiosrc", options.numChannels)
+    aTx = buildAudioSender(aConfig, options.ip, options.audioCodec, options.audioPort)
+
+    start() 
+    
+    # continue from here
+
+def runAsReceiver(options):
+    """ Receives media from a remote sender """
+    pass
+
+
+def main():
+    (options, args) = getArgs()
+
+    if options.isSender:
+        runAsSender(options)
+    else if not options.isSender:
+        runAsReceiver(options)
+    else:
+        raise PofExcept("Must specify if this process is a sender or receiver")
 
