cafd9e60a72b58ef728c50ad706cc2abf27fd4ac
Alexandre Quessy <alexandre@quessy.net>
2009-01-22 20:49:46 +0000

    devices: added one test

--- py/test/test_integration_devices.py
+++ py/test/test_integration_devices.py
@@ -37,7 +37,6 @@ app.main()
 app.me.verbose = False
 app.view.verbose = False
 
-
 class Test_devices_v4l2(unittest.TestCase):
     """
     Integration tests for v4l2 devices.
@@ -71,5 +70,26 @@ class Test_devices_v4l2(unittest.TestCase):
         app.api.device_modify_attribute(app.me, 'video', 'v4l2', '/dev/video0', 'norm', 'ntsc')
         app.go()
         
-
+    def test_03_width_height(self):
+        v4l2 = devices.get_manager('video').drivers['v4l2']
+        
+        try:
+            video0 = v4l2.devices['/dev/video0']
+        except KeyError, e:
+            self.fail('no device /dev/video0')
+        else:
+            attr_w = v4l2.devices['/dev/video0'].attributes['width']
+            attr_h = v4l2.devices['/dev/video0'].attributes['height']
+            for w, h in ((320, 240), (1024, 768), (640, 480)):
+                attr_w.set_value(w)
+                attr_h.set_value(h)
+                v4l2.poll_now()
+                app.go()
+                val_w, val_h = attr_w.get_value(), attr_h.get_value()
+                if val_w != w:
+                    self.fail('Width should has been changed to %d but is %d.' % (w, val_w))
+                elif val_h != h:
+                    self.fail('Height should has been changed to %d but is %d.' % (h, val_h))
+                else:
+                    pass
 
