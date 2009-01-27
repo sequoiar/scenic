2f5da9227b494273393341cc232288e15a567250
Alexandre Quessy <alexandre@quessy.net>
2009-01-27 01:18:00 +0000

    network: iperf improved (baby step)

--- py/network.py
+++ py/network.py
@@ -89,7 +89,7 @@ class IperfServerProcessProtocol(protocol.ProcessProtocol):
         #reactor.stop()
 
 
-def kill_process(process_transport, sig=15, verify_timeout=0.1):
+def kill_process(process_transport, sig=15, verify_timeout=1.0):
     """
     Kills a process started using reactor.spawnProcess
     Double checks after 0.1 second
@@ -112,6 +112,7 @@ def on_iperf_client_results(results, commands, extra_arg, caller):
     
     See utils.commands
     """
+    global state
     for i in range(len(results)): # should have a len() of 1
         result = results[i]
         success, results_infos = result
@@ -122,24 +123,79 @@ def on_iperf_client_results(results, commands, extra_arg, caller):
         else:
             stdout, stderr, signal_or_code = results_infos
             if success:
-                _parse_iperf_output(stdout.splitlines())
+                print "stdout:", stdout
+                iperf_stats = _parse_iperf_output(stdout.splitlines())
+                print iperf_stats
+                state = STOPPED
  
-def _parse_iperf_output(lines):
-    print "TODO: parse, change state and notify"
-    for line in lines:
-        print line
-        if line.endswith("%)"):
-            pass #percent = 
+class NetworkError(Exception):
+    """Any error due to iperf."""
+    pass
 
-def start_iperf_client(caller, server_addr, bandwidth_megabits=1):
+def _parse_iperf_output(lines):
     """
-    Starts  iperf -c localhost -u -b 1M
+    Parses the output of iperf -c <host> -u
+
+    Might raise NetworkError
+    """
+    ret = {}
+    keys = ['datetime', 'local ip', 'local port', 'remote ip', 'remote port', 'test index', 
+           'interval', 'transfer', 'bandwidth', 
+           'jitter', 'datagrams lost', 'total datagrams', 'loss',  'datagrams out of order']
+    #print "TODO: parse, change state and notify"
+    line_index = 0
+    for line in lines:
+        if line.find("WARNING: did not receive ack of last datagram after 10 tries.") != -1:
+            # error !!
+            raise NetworkError("Impossible to reach remote iperf server.")
+        if line.startswith("2"): # date
+            print line
+            if line_index == 0:
+                line_index += 1
+            else: # 2nd line is the only one we need
+                words = line.split(",")
+                word_index = 0
+                for word in words:# interval, transfert, jitter, packet loss (%)
+                    k = keys[word_index]
+                    if k == 'loss': 
+                        ret[k] = float(word) # %
+                    elif k == 'bandwidth': 
+                        ret[k] = int(word) # Kbits/sec
+                    elif k == 'jitter': 
+                        ret[k] = float(word) # ms
+                    elif k == 'loss': 
+                        ret[k] = int(word) # %
+                    word_index += 1
+    return ret
+
+# local  : datetime, local ip, local port, remote ip, remote port, test index, interval (0-10) sec, transfer (bytes), bandwidth (Mbit/sec)
+# remote  : datetime, local ip, local port, remote ip, remote port, test index, interval (0-10) sec, transfer (bytes), bandwidth (Mbit/sec), 
+#            jitter (ms), datagrams lost, total datagrams, % loss, datagrams out of order.
+#
+# WARNING: did not receive ack of last datagram after 10 tries.
+# if client tells this, unable to reach server.
+
+#alex@plank:~/src/miville/inhouse/py/network$ iperf -c brrr  -u -b 1M
+#------------------------------------------------------------
+#Client connecting to brrr, UDP port 5001
+#Sending 1470 byte datagrams
+#UDP buffer size:   109 KByte (default)
+#    ------------------------------------------------------------
+#    [  3] local 10.10.10.145 port 37468 connected with 10.10.10.65 port 5001
+#    [ ID] Interval       Transfer     Bandwidth
+#    [  3]  0.0-10.0 sec  1.19 MBytes  1000 Kbits/sec
+#    [  3] Sent 852 datagrams
+#    [  3] Server Report:
+#    [  3]  0.0-10.0 sec  1.19 MBytes  1000 Kbits/sec  0.006 ms    0/  852 (0%)
+def start_iperf_client(caller, server_addr, bandwidth_megabits=1, duration=10):
+    """
+    Starts  iperf -c localhost -u -t 10 -b 1M
     """
     global executable
     global state
-
+    
     if state == STOPPED:
-        command = ["iperf", "-c", server_addr, "-u", "-b", "%dM" % (bandwidth_megabits)] 
+        command = ["iperf", "-c", server_addr, "-t ", str(duration), "-y", "c", "-u", "-b", "%dM" % (bandwidth_megabits)] 
         #deferred = commands._command_start(executable, command)
         extra_arg = None
         callback = on_iperf_client_results
@@ -175,11 +231,19 @@ if __name__ == "__main__":
     if len(sys.argv) == 2:
         if sys.argv[1] == "client":
             print "starting client..."
-            start_iperf_client(None, "localhost")
+            start_iperf_client(None, "10.10.10.65") # "localhost")
         else:
             print "usage: <file name> client"
     else:
         start_iperf_server()
     reactor.callLater(1.0, debug_meanwhile)
     reactor.run()
+#    txt = """
+#    20090126182857,10.10.10.145,35875,10.10.10.65,5001,3,0.0-10.0,1252440,999997
+#    20090126182857,10.10.10.65,5001,10.10.10.145,35875,3,0.0-10.0,1252440,999970,0.008,0,852,0.000,0
+#    """
+#    
+#    lines = txt.strip().splitlines()
+#    pprint.pprint(_parse_iperf_output(lines))
+
 
