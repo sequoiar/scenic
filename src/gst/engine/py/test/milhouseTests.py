d8ce2fd663916c264770fdd417ec2cf36626ba9e
Tristan Matthews <tristan@sat.qc.ca>
2009-01-28 20:47:55 +0000

    quit milhouseTests with style and signals

--- src/gst/engine/py/test/milhouseTests.py
+++ src/gst/engine/py/test/milhouseTests.py
@@ -2,18 +2,36 @@
 
 import sys, os
 
+import signal
+
 from twisted.python.reflect import prefixedMethods
 
+pidRx = 0
+pidTx = 0
 
 class MilhouseTests():
+
     def __init__(self):
-        pass
+        signal.signal(signal.SIGINT, self.receiveInterrupt)
 
     @staticmethod
     def timeouts():
         """ Returns tuple of timeout arguments """
         timeout = '-o 5000 '
         return timeout, timeout
+
+    @staticmethod
+    def receiveInterrupt(signum, stack):
+        print 'Received interrupt, killing all child processes'
+        try: 
+            os.kill(pidRx, signal.SIGKILL)
+        except OSError:
+            pass
+        try:
+            os.kill(pidTx, signal.SIGKILL)
+        except OSError:
+            pass
+        sys.exit(0)
 
     @staticmethod
     def runTest(rxArgs, txArgs):
@@ -48,8 +66,13 @@ class MilhouseTests():
         for c in xrange(1, 9): 
             self.runTest(rxArgs, txArgs + '-c ' + str(c))
 
+    def test_03_dv(self):
+        """ Test dv inputs """
+        rxArgs, txArgs = self.timeouts()
+        self.runTest(rxArgs, txArgs + ' --videosource dv1394src --audiosource dv1394src')
+
 
 # here we run all the tests thanks to the wonders of reflective programming
-for test in prefixedMethods(MilhouseTests(), 'test_'):
+for test in prefixedMethods(MilhouseTests(), 'test_03'):
     test()
 
