0707c5214bf3241ef4056aec8cd741a65761f519
Tristan Matthews <tristan@sat.qc.ca>
2009-01-29 21:01:01 +0000

    py cleanup

--- src/gst/engine/py/gstPluginCheck.py
+++ src/gst/engine/py/gstPluginCheck.py
@@ -1,9 +1,27 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Sropulpof
+# Copyright (C) 2008 Société des arts technologiques (SAT)
+# http://www.sat.qc.ca
+# All rights reserved.
+#
+# This file is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Sropulpof is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Sropulpof.  If not, see <http:#www.gnu.org/licenses/>.
+#
 
 import os
 import gst
-
-
 
 GST_PLUGINS = ['level', 'audioconvert', 'alsasink', 'pulsesink', 'jackaudiosink', 
             'decodebin', 'capsfilter', 'dvdemux', 'queue', 'ffmpegcolorspace', 
@@ -25,11 +43,10 @@ for plug in GST_PLUGINS:
     else:
         print plug + " installed"
 
+print "-------------------------------"
 if missingPlugins == []:
-    print "-------------------------------"
     print "All necessary plugins installed"
 else:
-    print "-------------------------------"
     print "The following gstreamer plugins need to be installed: "
     for plug in missingPlugins:
         print plug
