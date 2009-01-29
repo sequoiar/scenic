253015c425929d7b47a29ba5ccac91aba93df7d7
Koya Charles <koya.charles@gmail.com>
2009-01-29 00:18:49 +0000

    simple tcp test py

---
+++ src/python/test.py
@@ -0,0 +1,8 @@
+from msgthreads import *
+class pycb(DictHandler):
+    def cb(self, d):
+        print d
+        return {"command":"ack"}
+c = TcpWrapConfig(1044,0)
+cb = pycb()
+t = ThreadWrap(c,cb)
