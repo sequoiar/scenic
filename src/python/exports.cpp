0d5c01d77e805dd15d928ea24537cbe521882912
Koya Charles <koya.charles@gmail.com>
2009-01-29 21:47:19 +0000

    python exports now all in src/python

--- src/python/exports.cpp
+++ src/python/exports.cpp
@@ -19,16 +19,19 @@
  */
 
 /** \file
- *      This file exposes class to python 
+ * This file exposes class to python 
  *
- *
- *      Exposes object modules to python interpreter.
+ * Exposes object modules to python interpreter, available by calling "from libmilhouse import *".
  *
  */
 
 #include <iostream>
 #include <Python.h>
 #include <boost/python.hpp>
+#include <gst/videoFactory.h>
+#include <gst/audioFactory.h>
+#include "util.h"
+#include "gutil.h"
 
 
 using namespace boost::python;
@@ -37,18 +40,61 @@ using namespace boost::python;
 #include "pyMsgThread.h"
 
 /// BOOST PYTHON MOD
-BOOST_PYTHON_MODULE(msgthreads)
+BOOST_PYTHON_MODULE(milhouse)
 {
-    class_ < dictMessageHandler, boost::noncopyable, boost::shared_ptr<HandlerWrapper> > ("DictHandler")
-        ;
-    class_ < MsgWrapConfig > ("MsgWrapConfig", no_init)
-        ;
-    class_ < TcpWrapConfig, bases<MsgWrapConfig> >("TcpWrapConfig",init <int, bool> ())
-        ;
-    class_ < GstWrapConfig, bases<MsgWrapConfig> >("GstWrapConfig")
-        ;
+
+    class_ < dictMessageHandler, boost::noncopyable, boost::shared_ptr<HandlerWrapper> > ("DictHandler") ;
+    class_ < MsgWrapConfig > ("MsgWrapConfig", no_init) ;
+    class_ < TcpWrapConfig, bases<MsgWrapConfig> >("TcpWrapConfig",init <int, bool> ()) ;
+    class_ < GstWrapConfig, bases<MsgWrapConfig> >("GstWrapConfig") ;
     class_ < ThreadWrap, boost::noncopyable > ("ThreadWrap",init < MsgWrapConfig*, dictMessageHandler* > ())
-        .def("send", &ThreadWrap::send)
-        ;
+        .def("send", &ThreadWrap::send) ;
+
+
+
+    //--- gst/engine
+    class_< VideoReceiver, boost::noncopyable, boost::shared_ptr<VideoReceiver> >("VideoReceiver", no_init)
+        .def("makeFullscreen", &VideoReceiver::makeFullscreen);
+    class_< VideoSender, boost::noncopyable, boost::shared_ptr<VideoSender> >("VideoSender", no_init)
+        .def("getCaps", &VideoSender::getCaps);     // methods
+        
+    def("buildVideoReceiver", videofactory::buildVideoReceiver);
+    def("buildVideoSender", videofactory::buildVideoSender);
+
+    class_< AudioReceiver, boost::noncopyable, boost::shared_ptr<AudioReceiver> >("AudioReceiver", no_init);
+    class_< AudioSender, boost::noncopyable, boost::shared_ptr<AudioSender> >("AudioSender", no_init)
+        .def("getCaps", &AudioSender::getCaps);     // methods
+        
+
+    def("buildAudioReceiver", audiofactory::buildAudioReceiver);
+    def("buildAudioSender", audiofactory::buildAudioSender);
+
+    class_< VideoSourceConfig >("VideoSourceConfig", init<std::string>()) 
+        .def(init<std::string, std::string>()); // overloaded constructor
+
+    class_< AudioSourceConfig >("AudioSourceConfig", init<std::string, int>()) 
+        .def(init<std::string, std::string, int>()); // overloaded constructor
+
+    def("tcpSendBuffer", tcpSendBuffer);        
+
+    def("start", playback::start);
+    def("stop", playback::stop);
+    def("isPlaying", playback::isPlaying);
+
+    def("eventLoop", gutil::runMainLoop);
+    def("setHandler", set_handler);
+    
+    boost::python::scope().attr("VIDEO_PORT") = ports::V_PORT;
+    boost::python::scope().attr("AUDIO_PORT") = ports::A_PORT;
+
+    boost::python::scope().attr("VIDEO_CAPS_PORT") = ports::VIDEO_CAPS_PORT;
+    boost::python::scope().attr("AUDIO_CAPS_PORT") = ports::AUDIO_CAPS_PORT;
+
+    boost::python::scope().attr("VIDEO_MSG_ID") = videofactory::MSG_ID;
+    boost::python::scope().attr("AUDIO_MSG_ID") = audiofactory::MSG_ID;
+
+    // expose macro to python
+    boost::python::scope().attr("PACKAGE_VERSION") = PACKAGE_VERSION;
+    boost::python::scope().attr("RELEASE_CANDIDATE") = RELEASE_CANDIDATE;
 }
 
