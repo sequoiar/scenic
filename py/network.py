626a5c82d58ce1cfb1aa0df6483c719076134c93
Alexandre Quessy <alexandre@quessy.net>
2009-02-13 21:00:50 +0000

    network: some progress

--- py/network.py
+++ py/network.py
@@ -52,147 +52,6 @@ STATE_QUERIED_FOR_DUALTEST = 5
 STATE_WAITING_REMOTE_ANSWER = 6
 STATE_ANSWERED_OK = 7
 
-# --------------------------------- server side ---------------------
-class TestServerProtocol(basic.LineReceiver):
-    """
-    server side communication protocol
-
-    usage :
-    * quit
-    * test <float ms> args
-    * ping
-    """
-    def connectionMade(self):
-        print "Connection from", self.transport.getPeer().host
-
-    def lineReceived(self, line):
-        if line == 'quit':
-            self.sendLine("Goodbye.")
-            self.transport.loseConnection()
-        elif line.startswith("test"):
-            args = line.split(" ")[1:]
-            print "server received test with args : ", args
-            self.sendLine("starting test")
-        elif line == "ping":
-            print "server received ping"
-            self.sendLine("pong")
-        else:
-            self.sendLine("You said: " + line)
-
-class TestServerFactory(protocol.ServerFactory):
-    protocol  = TestServerProtocol
-# ------------------------------------- client side ----------------
-class TestClientProtocol(basic.LineReceiver):
-    """
-    client side protocol for network tests
-
-    usage : 
-    * starting test
-    * pong 
-    """
-    def __init__(self):
-        self.ping_started = 0.0000 # time.time()
-
-    def lineReceived(self, line):
-        #log.debug('Line received from %s:%s: %s' % (self.transport.realAddress[0], self.transport.realAddress[1], line))
-        #log.info('Bad command receive from remote')
-        #print "client received ", line
-
-        if line == "starting test":
-            print "client received starting test"
-            self.sendLine("quit")
-            #self.transport.loseConnection()
-
-        elif line == "pong":
-            print "client received pong"
-            latency = (time.time() - self.ping_started) * 1000 # roughly estimated in ms
-            print "latency : %f ms" % latency 
-            latency = latency / 2.0 # unidirectional
-            self.sendLine("test %f" % latency)
-
-    def connectionMade(self):
-        self.factory.deferred.callback("Connected !")
-        self.ping_started = time.time()
-        self.sendLine("ping")
-
-    def connectionLost(self, reason):
-        """
-        The reason Failure wraps a twisted.internet.error.ConnectionDone or twisted.internet.error.ConnectionLost instance
-        """
-        print "client : connectionLost"
-        try:
-            print reason.message
-        except AttributeError:
-            pass
-        #reactor.stop()
-
-class TestClientFactory(protocol.ClientFactory):
-    protocol = TestClientProtocol
-    def __init__(self):
-        self.deferred = defer.Deferred()
-
-    def clientConnectionFailed(self, connector, reason):
-        self.deferred.errBack(reason)
-
-class TestClient:
-    """
-    The actual client for network tests
-    
-    This is the class that does the job.
-    """
-    # constants 
-    DISCONNECTED = 0
-    CONNECTED = 1
-
-    def __init__(self): #, contact, api):
-        self._state = self.DISCONNECTED
-
-    def connect_to(self, host, port):
-        """
-        Connects to a remote TestServerFactory
-
-        :return: a defer.Deferred instance.
-        """
-        test_factory = TestClientFactory()
-        reactor.connectTCP(host, port, test_factory)
-        test_factory.deferred.addCallback(self.handle_success, host, port)
-        test_factory.deferred.addErrback(self.handle_failure, host, port)
-        return test_factory.deferred
-
-    def handle_success(self, result, host, port):
-        """
-        Callback for successful connection to a remote test server
-        """
-        print "connected to port %i" % port
-        self.state = self.CONNECTED
-
-    def handle_failure(self, failure, host, port):
-        """
-        Errback for a failure when trying to connect to a remore test server.
-        """
-        print "error connecting to port %i" % port
-        print failure.getErrorMessage()
-        # reactor.stop()
-
-
-# -------------------------------- iperf --------------------------------------
-# local  : datetime, local ip, local port, remote ip, remote port, test index, interval (0-10) sec, transfer (bytes), bandwidth (Mbit/sec)
-# remote  : datetime, local ip, local port, remote ip, remote port, test index, interval (0-10) sec, transfer (bytes), bandwidth (Mbit/sec), 
-#            jitter (ms), datagrams lost, total datagrams, % loss, datagrams out of order.
-#
-# WARNING: did not receive ack of last datagram after 10 tries.
-# if client tells this, unable to reach server.
-#
-# aalex@plank:~$ iperf -c localhost -u -y c -t 1 -b 1M
-# 20090130142704,127.0.0.1,35745,127.0.0.1,5001,3,0.0-1.0,127890,999957
-# 20090130142704,127.0.0.1,5001,127.0.0.1,35745,3,0.0-1.0,127890,1000047,0.005,0,87,0.000,0
-
-# actually in the stdout
-# 20090130143525,10.10.10.145,57442,10.10.10.65,5001,3,0.0-1.0,127890,999964
-# 20090130143525,10.10.10.65,5001,10.10.10.145,57442,3,0.0-1.0,127890,1000013,0.010,0,87,0.000,0
-# --------------------- constants --------------------------------
-NETPERF_TEST_UNIDIRECTIONAL = "unidirectional from local to remote"
-
 # functions ------------------------------------------------------
 def _parse_iperf_output(lines):
     """
@@ -230,41 +89,59 @@ def _parse_iperf_output(lines):
                     word_index += 1
     return ret
 
-class IperfServerProcessProtocol(protocol.ProcessProtocol):
-    """
-    Manages the process of the `iperf -s -u` command
-    """
-    def __init__(self):
-        pass
-
-    def connectionMade(self):
-        pass # print "connectionMade"
-
-    def outReceived(self, data):
-        print data
-
-    def errReceived(self, data):
-        pass # print "errReceived! with %d bytes!" % len(data)
-
-    def inConnectionLost(self):
-        pass # print "inConnectionLost! stdin is closed! (we probably did it)"
-
-    def outConnectionLost(self):
-        pass # print "outConnectionLost! The child closed their stdout!"
-
-    def errConnectionLost(self):
-        pass # print "errConnectionLost! The child closed their stderr."
-
-    def processEnded(self, status_object):
-        print "Ended iperf server, status %d" % status_object.value.exitCode
-        #print "STOP REACTOR"
-        #reactor.stop()
-
 class NetworkError(Exception):
     """
     Any error due to network testing (with iperf).
     """
     pass
+
+class IperfServerProcessProtocol(protocol.ProcessProtocol):
+    """
+    Manages the process of the `iperf -s -u` command
+    """
+    def __init__(self):
+        self.network_tester = None # which started this process
+        self.verbose = True
+        self.prefix = "iperf -s>"
+        self.network_tester_warned = False
+    
+    def connectionMade(self):
+        if self.verbose:
+            print self.prefix, "connectionMade"
+    
+    def outReceived(self, data):
+        if self.verbose:
+            for line in data.splitlines():
+                print self.prefix, line
+
+    def _warn_network_tester_that_i_died(self, was_intentional=False):
+        if not self.network_tester_warned:
+            self.network_tester_warned = True
+            self.network_tester.on_iperf_server_process_done()
+
+    def inConnectionLost(self):
+        pass # print "inConnectionLost! stdin is closed! (we probably did it)"
+        self._warn_network_tester_that_i_died()
+    
+    def errConnectionLost(self):
+        # print "errConnectionLost! The child closed their stderr."
+        self._warn_network_tester_that_i_died()
+
+    def processEnded(self, status):
+        print self.prefix, "Ended iperf server, status %d" % status.value.exitCode
+        #print "STOP REACTOR"
+        #reactor.stop()
+        try:
+            if status_object.value.exitCode == 1:
+                pass # TODO
+        except:
+            pass
+        try:
+            print self.prefix, status.getErrorMessage()
+        except:
+            pass
+        self._warn_network_tester_that_i_died()
+
 
 # --------------- states : ------------
 #STATE_IDLE = 0
@@ -283,11 +160,6 @@ class NetworkTester(object):
     The `iperf` command can be used either as a client or a server.
     It is the client that initiates the testing.
     """
-    # constants
-    STOPPED = 0
-    SERVER_RUNNING = 1
-    CLIENT_RUNNING = 2 
-    
     def __init__(self):
         # globals
         self.iperf_server = None
@@ -302,6 +174,11 @@ class NetworkTester(object):
         self.current_caller = None # instance
         self.current_stats_local = None # dict
         self.current_stats_remote = None # dict
+        self.current_com_chan = None # com_chan
+        self.current_kind = None
+        self.current_latency = 0 # milliseconds
+        self.current_ping_time = 0 # UNIX timestamp
+
         # -------- settings
         self.accept_timeout = 20 # timeout before auto rejecting dualtest query
         self.remote_results_timeout = 5 # how many extra seconds over the duration of a test to wait for remote stats before giving up.
@@ -313,9 +190,10 @@ class NetworkTester(object):
         
         Used in this case to kill the iperf server.
         """
+        process_transport = self.iperf_server
         try:
             process_transport.signalProcess(sig)
-            self.state = STATE_IDLE
+            self.iperf_server_is_running = False
         except ProcessExitedAlready, e:
             print "error : ProcessExitedAlready (may be ok)", e.message
             self.state = STATE_IDLE
@@ -324,7 +202,17 @@ class NetworkTester(object):
                 reactor.callLater(verify_timeout, self.kill_server_process, process_transport, 9)
             else: # sig = 9 (second time)
                 process_transport.loseConnection()
-                self.state = STATE_IDLE # STOPPED
+                self.iperf_server_is_running = False
+        #TODO: make sure we do not start again the process !!!!!!!!!!!
+
+    def on_iperf_server_process_done(self, intentional=True):
+        """
+        Called when the iperf -s process is done.
+        """
+        self.iperf_server_is_running = False
+        if not intentional:
+            print "iperf server process died unintentionally. will start it again." # TODO: log
+            reactor.callLater(1.0, self._start_iperf_server_process)
 
     def on_iperf_command_results(self, results, commands, extra_arg, caller):
         """
@@ -350,15 +238,15 @@ class NetworkTester(object):
                             # TODO
                             raise NetworkError("iperf command gave unexpected results. Is remote iperf running ?")
                     except NetworkError, e:
-                        self.notify_api(caller, 'info', e.message)
+                        self.notify_api(caller, 'error', e.message)
                     else:    
-                        iperf_stats.update(extra_arg)
-                        self.state = self.STOPPED
+                        iperf_stats.update(extra_arg) # appends the infos to the results dict
+                        self.state = STATE_IDLE 
 
-                        self.notify_api(caller, 'info', "iperf stats ifor IP %s: %s" % (ip, str(iperf_stats)))
+                        self.notify_api(caller, 'info', "iperf stats for IP %s: %s" % (ip, str(iperf_stats)))
                         self.notify_api(caller, "network_test_done", iperf_stats)
                 else:
-                    self.notify_api(caller, "info", "network performance : Unknown error.") # TODO use error key
+                    self.notify_api(caller, "error", "network performance : Unknown error.") # TODO use error key
 
     def notify_api(self, caller, key, val):
         """
@@ -378,67 +266,86 @@ class NetworkTester(object):
         else:
             self.api.notify(caller, val, key) #key, res)
 
-    def start_bidirectional_as_client(self, caller, remote_addr, bandwidth_megabits=1, duration=10):
-        # TODO
-        if self.state == self.STOPPED:
-            
-           self._on_bidirectional_ok(caller, remote_addr, remote_addr, bandwidth_megabits, duration)
-        else:
-            self.notify_api(caller, "error", "Network test is already happening.")
-
-    def _on_bidirectional_ok(self, caller, server_addr, bandwidth_megabits=1, duration=10):
+    def _on_dualtest_ok(self, caller, server_addr, bandwidth_megabits=1, duration=10):
         # TODO
         """
         Called when remote server has said it would be ok to start the iperf bidirectional test.
         """
-        self.start_test(caller, server_addr, bandwidth_megabits, duration)
+        reactor.callLater(self.current_latency / 2.0, self._start_iperf_client)
+        
 
-    def start_test(self, caller, server_addr, bandwidth_megabits=1, duration=10): # start_client
+    def start_test(self, caller, server_addr, bandwidth_megabits=1, duration=10, kind=KIND_UNIDIRECTIONAL, com_chan=None): # start_client
         """
         Starts `iperf -c localhost -y c -u -t 10 -b 1M`
-        
+       
+        Actually, the process is started in self._start_iperf_client()
         duration : int
         bandwidth_megabits : int
         server_addr: str
         """
-        if self.state == self.STOPPED:
-            command = ["iperf", "-c", server_addr, "-t", "%d" % (duration), "-y", "c", "-u", "-b", "%dM" % (bandwidth_megabits)] 
-            extra_arg = {
-                'ip': server_addr, 
-                'bandwidth': bandwidth_megabits, 
-                'duration': duration, 
-                'kind': NETPERF_TEST_UNIDIRECTIONAL # TODO
-            }
-            callback = self.on_iperf_command_results
-            try:
-                commands.single_command_start(command, callback, extra_arg, caller)
-            except CommandNotFoundError, e: # TODO: 
-                print "error: ", e.message
+        if self.state == STATE_IDLE:
+            self.current_remote_addr = server_addr
+            self.current_bandwidth = bandwidth_megabits # Mbits
+            self.current_duration = duration # seconds
+            self.current_caller = caller # instance
+            self.current_com_chan = com_chan # only neededi for dualtests
+            self.current_stats_local = None # dict
+            self.current_stats_remote = None # dict
+            self.current_kind = kind
+            
+            if kind in (KIND_UNIDIRECTIONAL, KIND_TRADEOFF):
+                self._start_iperf_client()
             else:
-               self.state = self.CLIENT_RUNNING
+                # KIND_DUALTEST
+                # TODO
+                pass
         else:
             self.notify_api(caller, "error", "Network test is already happening.")
-    
+
     def _start_iperf_client(self):
         """
         this is where we actually start the iperf process as a client
         """
-        
+        command = ["iperf", 
+            "-c", self.current_remote_addr, # ip
+            "-t", "%d" % self.current_duration, # duration
+            "-y", "c", # CSV output
+            "-u", # UDP
+            "-b", "%dM" % self.current_bandwidth # bandwidth (megabits)
+        ] 
+        if self.current_kind == KIND_TRADEOFF:
+            command.append("-r") # tradeoff.
+            # all other tests 
+        #extra_arg = None
+        extra_arg = {
+            'ip': self.current_remote_addr, #server_addr, 
+            'bandwidth': self.current_bandwidth, # bandwidth_megabits, 
+            'duration': self.current_duration, 
+            'kind': self.current_kind # NETPERF_TEST_UNIDIRECTIONAL # TODO
+        }
+        callback = self.on_iperf_command_results
+        try:
+            commands.single_command_start(command, callback, extra_arg, self.current_caller)
+        except CommandNotFoundError, e: # TODO: 
+            print "error: ", e.message
+        else:
+           self.state = self.current_kind # set the state to the current kind
 
     def _start_iperf_server_process(self):
         """
         Starts `iperf -s -u`
         """
-        if self.state == self.STOPPED:
+        if self.state == STATE_IDLE: 
             proto = IperfServerProcessProtocol()
+            proto.network_tester = self
             try:
-                self.iperf_server = reactor.spawnProcess(proto, "/usr/bin/iperf", ["/usr/bin/iperf", "-s", "-u"], os.environ)
+                self.iperf_server = reactor.spawnProcess(proto,\
+                        "/usr/bin/iperf", ["/usr/bin/iperf", "-s", "-u"],\
+                        os.environ)
             except OSError, e:
                 print "Error starting process: ", e 
             else:
                 self.iperf_server_is_running = True
-                #self.state = self.SERVER_RUNNING
-                #reactor.callLater(15.0, self.kill_server_process, self.iperf_server)
     
 # functions ---------------------------------------------
 def start(subject):
@@ -448,7 +355,6 @@ def start(subject):
     Notifies with 'error' key  if `iperf` command not found. (CommandNotFoundError)
     Returns a NetworkTester instance with the server started.
     """
-    # will raise CommandNotFoundError if not found:
     try:
         executable = commands.find_command("iperf", 
             "`iperf` command not found. Please see See https://svn.sat.qc.ca/trac/miville/wiki/NetworkTesting for installation instructions.")
@@ -472,51 +378,4 @@ def start(subject):
     #    
     #    lines = txt.strip().splitlines()
     #    pprint.pprint(_parse_iperf_output(lines))
-
-# -------------------------- MAIN ---------------------------------
-if __name__ == "__main__":
-    if True: #False: # ------------------- test iperf
-        # SIMPLE TEST
-        def debug_meanwhile():
-            # TODO : remove
-            print "waiting..."
-            reactor.callLater(0.5, debug_meanwhile)
-        
-        # default : client
-        
-        iperf = NetworkTester()
-        if len(sys.argv) == 2:
-            if sys.argv[1] == "server":
-                iperf._start_iperf_server_process()
-            else:
-                print "usage: <file name> [server]"
-        else:
-            print "starting client..."
-            iperf.start_test("Dummy caller", "10.10.10.66", 1, 1) # "localhost" | tzing !
-        reactor.callLater(0.5, debug_meanwhile)
-        reactor.callLater(2.0, lambda: reactor.stop())
-    #    try:
-        reactor.run()
-    #    except KeyboardInterrupt:
-    #        print "you pressed ctrl-c"
-    #        reactor.stop() 
-        print "-----------------------------------"
-        print "reactor has stopped"
-    else: # ---------- test network protocol
-        # ----------------------------------------------------------------
-        # and now, the test for the network test protocol.
-        port = 15432
-        host = "localhost"
-        reactor.listenTCP(port, TestServerFactory())
-        print "Server running on port %d, press ctrl-C to stop." % (port)
-        print "starting client"
-        client = TestClient()
-        client.connect_to(host, port)
-        #reactor.callLater(2.0, lambda: reactor.stop())
-        #try: 
-        reactor.run()
-        #except KeyboardInterrupt:
-        #    print "you pressed ctrl-c"
-        #    reactor.stop() 
-        print "reactor has stopped"
 
