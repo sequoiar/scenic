b98bb0f7e13d117fbe67f36ef38a8d805761105c
Simon Piette <simonp@sat.qc.ca>
2009-02-20 22:32:49 +0000

    Greatest commit evar!

--- src/python/test.py
+++ src/python/test.py
@@ -1,8 +1,24 @@
-from milhouse import *
-class pycb(DictHandler):
-    def cb(self, d):
-        print d
+#!/usr/bin/env python
+
+import sys
+sys.path.append('.libs')
+from milhouse import TcpWrapConfig
+
+class Pycb(DictHandler):
+    """
+    Basic test for TCP wrapper
+    """
+    def __init__(self):
+        """ nothing """
+        pass
+
+    def callback(self, message):
+        """
+        Callback from TCP
+        """
+        print message
         return {"command":"ack"}
-c = TcpWrapConfig(1044,0)
-cb = pycb()
+
+Config = TcpWrapConfig(1044, 0)
+Callback_tested = Pycb()
 #t = ThreadWrap(c,cb)
