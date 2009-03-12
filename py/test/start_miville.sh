9867754604eab33f0ac291c3208346c12f7f7ccc
Simon Piette <simonp@sat.qc.ca>
2009-03-12 14:18:59 +0000

    Minor edits on start_miville.sh

--- py/test/start_miville.sh
+++ py/test/start_miville.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Licensed under GPLv2 or later
 
 # This script starts processes needed by tests
 # We need:
@@ -6,31 +7,31 @@
 # A running miville
 # An available display
 # To get the display available, you need to be locally logged on 
+
+# Get MIVILLE_PATH or assign a default value
 MIVILLE_PATH=${MIVILLE_PATH:-$HOME/src/miville/trunk/py}
 
-
-if  [ -d /dev/shm/jack* ]; then
-    if [ ! -w /dev/shm/jack*/default ]; then
-        echo 'Another user is running jackd'
-        ls -ld /dev/shm/jack*
+if  [ -d /dev/shm/jack-* ]; then
+    if [ ! -w /dev/shm/jack-* ]; then
+        echo 'User $(stat -c %U /dev/shm/jack*) is running jackd'
         exit 1
     fi
 fi
 
-if [ ! -d /dev/shm/jack*/default ]; then
+if [ ! -d /dev/shm/jack-* ]; then
     echo "No jackd running"
     if [ ! -r ~/.jackdrc ]; then
         echo "You must have a ~/.jackdrc. Run qjackctl to create one."
         exit 1
     fi
-    echo $(cat ~/.jackdrc) &
+    $(cat ~/.jackdrc) &
 fi
 
 # Now we test the DISPLAY
 export DISPLAY=:0.0
 xdpyinfo >/dev/null
 if [ $? -eq 1 ]; then
-    echo "DISPLAY can't be used. Be sure to log on locally."
+    echo "DISPLAY $DISPLAY can't be used. Be sure to log on locally."
     exit 1
 fi
 
@@ -38,9 +39,9 @@ fi
 ps aux | grep -q [m]ivillle.py
 if [ $? -eq 1 ]; then
     cd $MIVILLE_PATH
-    echo ./miville.py &
+    ./miville.py
 else
-    user_running_miville=$(ps aux | awk '/[m]ivillelll.py/ { print $1 }')
+    user_running_miville=$(ps aux | awk '/[m]iville.py/ { print $1 }')
     if [ "x$user_running_miville" != "x$(whoami)" ]; then
         echo "$user_running_miville is already running miville"
         exit 1
