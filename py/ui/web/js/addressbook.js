80e973e8de729cc7c71a10e6f14be03f346a43be
Étienne Désautels <etienne@teknozen.net>
2009-03-16 22:03:19 +0000

    Refactoring and code cleanup in javascript.

--- py/ui/web/js/addressbook.js
+++ py/ui/web/js/addressbook.js
@@ -17,11 +17,12 @@ Addressbook.methods(
 		self.adb_address = $('adb_address');
 		self.adb_port = $('adb_port');
 		self.adb_fields = [self.adb_name, self.adb_address, self.adb_port]
+		self.adb_add = $('adb_add');
 		self.adb_remove = $('adb_remove');
 		self.adb_join = $('adb_join');
 		self.adb_status = $('adb_status');
 
-		// get translations (maybe we can do this automatically?)
+		// get string translations (maybe we can do this automatically?)
 		self.save_str = $('js_adb_save').get('text');
 		self.edit_str = $('js_adb_edit').get('text');
 		self.join_str = $('js_adb_join').get('text');
@@ -31,15 +32,15 @@ Addressbook.methods(
 		self.adb_edit.value = self.edit_str;
 		self.adb_join.value = self.join_str;
 
-		// get the list
-        self.callRemote("rc_get_list");
-		
-		// create new base element
+		// create new base elements
 		self.conn_icon = new Element('div', {
 			'class': 'conn_icon'
 		});
-		self.status_div = new Element('div');
+				
+		// get the contact list
+        self.callRemote("rc_get_list");
 		
+		// create the empty field validator
 		self.isEmpty = new InputValidator('required', {
 		    errorMsg: 'This field is required.',
 		    test: function(field) {
@@ -47,6 +48,7 @@ Addressbook.methods(
 		    }
 		});
 		
+		// add some keyboard events to the info fields 
 		self.adb_fields.each(function(elem) {
 			elem.addEvent('enter', function(event){
 				self.editSave();
@@ -56,6 +58,9 @@ Addressbook.methods(
 				self.cancelEdit();
 			});
 		});
+		
+		// register to the widgets communicator
+		register('adb', self);
     },  
 
 
@@ -63,27 +68,45 @@ Addressbook.methods(
 	/* Call from Server */
 	//////////////////////
 
+	/* Update the contact list to reflect the state of the server */
+	/* Changed only what was change */
     function updateList(self, contacts) {
+		// maybe we will have to deal with the scroll position in the future
 		// dbug.info(self.adb_list.getScroll().y);
-//		self.adb_list.empty();
 		var counter = 0;
+		// previous updated contact (li element)
 		var previous = null;
+		// current selected li element
 		var selected = null;
+		
+		// list of contact names coming from the server
 		var server_list = contacts.map(function(item, index){
 			return item[0];
 		});
+		
+		// list of contact names on the client side
 		var client_list = self.adb_list.getChildren('li').get('name');
+		
+		// remove deleted contacts on the client side 
 		client_list.each(function(item){
 			if (item && !server_list.contains(item)) {
 				self.adb_list.getElement('li[name=' + item + ']').dispose();
 			}
 		});
+		
+		// add/update each contact from the server list
 		contacts.each(function(item){
-			counter += 1;
 			dbug.info(item)
+
+			// counter to know if the row are odd or even
+			counter += 1;
+			
+			// update info
 			if (client_list.contains(item[0])) {
 				var li = self.adb_list.getElement('li[name=' + item[0] + ']');
 				li.set('state', item[1]);
+				
+			// add the new contact
 			} else {
 				var li = new Element('li', {
 					'class': 'color_selectable',
@@ -94,35 +117,20 @@ Addressbook.methods(
 					'status': '',
 					'error': '',
 					'events': {
-						'click': function() {	// should be move in the client section
-					self.adb_list.getElements('li.color_selected').removeClass('color_selected')
-					this.addClass('color_selected');
-							var name = this.get('name');
-							self.callRemote('rc_get_contact', name);
-							self.adb_edit.disabled = false;
-							var state = this.get('state');
-							self.joinState(state);
-							self.adb_status.set('text', this.get('status'));
-							self.adb_status.set('title', this.get('error'));
-							self.adb_remove.removeEvents('click');
-							self.adb_remove.addClass('button_disabled');
-							if (state == 0) {
-								self.adb_remove.removeClass('button_disabled');
-								self.adb_remove.addEvent('click', function(){
-									self.remove();
-								});
-							}
-							self.selected = name;
-							self.cleanFields();
-							Cookie.write('adb_selected', name, {duration: 365});						
+						'click': function() {
+							self.contact_selected(this);
 						},
 						'dblclick': function() {
 							self.editSave();
 						}
 					}
 				});
+				
+				// add chat and state icons
 				self.conn_icon.clone().inject(li, 'top');
 				self.conn_icon.clone().inject(li, 'top');
+				
+				// insert it at the write place
 				if (previous) {
 					li.inject(previous, 'after');
 				} else if (counter == 1) {
@@ -131,14 +139,17 @@ Addressbook.methods(
 					li.inject(self.adb_list);
 				}
 			}
+			// keep the current position
 			previous = li;
 			
+			// deal with row coloring
 			if (counter % 2) {
 				li.addClass('color_zebra');
 			} else {
 				li.removeClass('color_zebra');
 			}
-		
+
+			// update the connection state icon
 			var conn_state = li.getChildren()[0];
 			conn_state.removeClass('spinner_small');
 			conn_state.removeClass('conn_connected');
@@ -148,15 +159,17 @@ Addressbook.methods(
 				conn_state.addClass('conn_connected');
 			}
 			
+			// keep the current selected li element
 			if (item[0] == self.selected) {
 				selected = li;
 			}
-
 		})
 
 		if (selected){
+			// update the display of selected contact info and button states
 			selected.fireEvent('click');
 		} else {
+			// or if no selection, clear the info and update button states 
 			self.adb_fields.each(function(elem) {
 				elem.value = '';
 			});
@@ -186,6 +199,7 @@ Addressbook.methods(
 	},
 
 	function status(self, contact, msg, details) {
+		// check if owner is null
 		var owner = self.adb_list.getElement('li[name=' + contact + ']')
 		owner.set('status', msg);
 		if (details == null) {
@@ -209,6 +223,66 @@ Addressbook.methods(
 	/* Call from Client */
 	//////////////////////
 	
+	// get info from others widgets
+	function update(self, caller, call, data) {
+		dbug.info(caller);
+		dbug.info(call);
+		dbug.info(data);
+	},
+
+	// update the interface in function of the selected contact
+	function contact_selected(self, contact) {
+		// updated the selection color
+		var curr_selection = self.adb_list.getElements('li.color_selected')
+		if (curr_selection) {
+			curr_selection.removeClass('color_selected');
+		}
+		contact.addClass('color_selected');
+		
+		// get the contact infos
+		var name = contact.get('name');
+		self.callRemote('rc_get_contact', name);
+		
+		// update the buttons state
+		var state = contact.get('state');
+
+		self.add_state('enable');
+		self.remove_state('enable');
+		
+		self.adb_edit.disabled = state;
+		self.joinState(state);
+		self.adb_status.set('text', contact.get('status'));
+		self.adb_status.set('title', contact.get('error'));
+		self.adb_remove.removeEvents('click');
+		self.adb_remove.addClass('button_disabled');
+		if (state == 0) {
+			self.adb_remove.removeClass('button_disabled');
+			self.adb_remove.addEvent('click', function(){
+				self.remove();
+			});
+		}
+		self.cleanFields();
+		
+		// save the selected contact name
+		self.selected = name;
+		Cookie.write('adb_selected', name, {duration: 365});
+		
+		// notify the others widgets of this selection
+		notify('adb', 'selection', self.selected);
+	},
+
+	// manage add button state
+	function add_state(self, state) {
+		(state == 'enable') ? state=false : state=true
+		self.adb_add.disabled = state;
+	},
+
+	// manage remove button state
+	function remove_state(self, state) {
+		(state == 'enable') ? state=false : state=true
+		self.adb_remove.disabled = state;
+	},
+
 	function joinState(self, state) {
 		if (state == 0) {
 			self.adb_join.disabled = false;							
--- py/ui/web/js/utils.js
+++ py/ui/web/js/utils.js
@@ -1,7 +1,29 @@
 
-///////////////////
-/* General stuff */
-///////////////////
+////////////////////////////////
+/* Inter widget communication */
+////////////////////////////////
+
+// List of registered widgets
+var widgets = new Hash();
+
+// Register a widget
+function register(name, widget) {
+	widgets.set(name, widget);
+}
+
+// Notify all registered widgets (except the caller) of this event
+function notify(caller, call, data) {
+	widgets.each(function(widget, name) {
+		if (caller != name) {
+			widget.update(caller, call, data);
+		}
+	});
+}
+
+
+/////////////////
+/* Event stuff */
+/////////////////
 
 // Extend Element.Events to add a event for the enter/return key
 Element.Events.enter = {
