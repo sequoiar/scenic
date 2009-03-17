57c9230514a38dc1a545a16d961bb6ef7990949a
Alexandre Quessy <alexandre@quessy.net>
2009-03-17 23:12:52 +0000

    adding the miville executable. py/ will eventually be the miville package

---
+++ py/__init__.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Sropulpof
+# Copyright (C) 2008 Société des arts technologiques (SAT)
+# http://www.sat.qc.ca
+# All rights reserved.
+#
+# This file is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Sropulpof is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Sropulpof.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Miville Python package header file.
+
+A Python package is a directory containing related modules files together.
+
+At installation time, the "py" directory is renamed to 
+/usr/local/lib/python2.5/site-packages/miville
+"""
+
+import miville
+
+# for when calling 'from <package> import *'
+__all__ = ['miville']
+
