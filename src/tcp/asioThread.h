4fc430f524b90ede935c8cb16f1ff0cb20b881a3
Koya Charles <koya.charles@gmail.com>
2009-03-21 17:12:06 +0000

    cleanup and License

--- src/tcp/asioThread.h
+++ src/tcp/asioThread.h
@@ -1,13 +1,27 @@
-//
-// async_tcp_echo_server.cpp
-// ~~~~~~~~~~~~~~~~~~~~~~~~~
-//
-// Copyright (c) 2003-2008 Christopher M. Kohlhoff (chris at kohlhoff dot com)
-//
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
+/* asioThread.h
+ * Copyright (C) 2009 Société des arts technologiques (SAT)
+ * http://www.sat.qc.ca
+ * All rights reserved.
+ *
+ * This file is part of [propulse]ART.
+ *
+ * [propulse]ART is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * [propulse]ART is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with [propulse]ART.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
+#ifndef _ASIO_THREAD_H_
+#define _ASIO_THREAD_H_
 #include <cstdlib>
 #include <iostream>
 
@@ -18,3 +32,4 @@ class asio_thread : public MsgThread
     int main();
 };
 
+#endif //_ASIO_THREAD_H_
