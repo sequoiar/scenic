a017ffa9a955f00d1763e624c3e557945efe8dda
Alexandre Quessy <alexandre@quessy.net>
2009-03-24 17:49:38 +0000

    firereset: improved errors messages.

--- src/devices/firereset.c
+++ src/devices/firereset.c
@@ -1,4 +1,4 @@
-// Sropulpof
+// Miville
 // Copyright (C) 2008 Société des arts technologiques (SAT)
 // http://www.sat.qc.ca
 // All rights reserved.
@@ -8,13 +8,13 @@
 // the Free Software Foundation, either version 2 of the License, or
 // (at your option) any later version.
 //
-// Sropulpof is distributed in the hope that it will be useful,
+// Miville is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Sropulpof.  If not, see <http://www.gnu.org/licenses/>.
+// along with Miville.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
  * Raw1394 (Firewire on GNU/Linux) bus reset.
@@ -22,17 +22,23 @@
  * to compile : 
  * gcc -Wall -lraw1394 -o resetbus resetbus.c
  */
+// TODO: add bus number argument
+
 //#include <unistd.h> // usleep
 #include <stdio.h> // printf
 #include <libraw1394/raw1394.h>
 
 static int bus_reset_handler(struct raw1394_handle *handle, unsigned int gen)
 {
-    printf("SUCCESS !!!!!!!!!\n");
-    printf("Bus reset occurred.\nnew generation number: %d, %d nodes on the bus, local ID: %d\a\n",
+    /**
+     * Callback for when raw1394_reset_bus_new() is called.
+     */
+    printf("INFO: Bus reset occurred.\nnew generation number: %d, %d nodes on the bus, local ID: %d\a\n",
         gen, 
         raw1394_get_nodecount(handle), 
         raw1394_get_local_id(handle) & 0x3f);
+    printf("SUCCESS !\n");
+    // TODO: check for errors ?
     raw1394_update_generation(handle, gen);
     return 0;
 }
@@ -45,31 +51,42 @@ int main(int argc, char **argv)
     int type = RAW1394_LONG_RESET;
     int adapter_number = 0; // see firecontrol/commander.c
     
+    if (argc >= 2)
+    {
+        printf("Usage: %s\n", argv[0]);
+        printf("Resets the Firewire (ieee1394) bus.\n");
+        return 0;
+    }
+
     handle = raw1394_new_handle();
     if (handle == 0)
     {
-        printf("could not get handle to firewire bus\n");
-        printf("Is raw1394 module loaded ?\nIs your user in the 'disk' group ?");
+        printf("ERROR: could not get handle to firewire bus.\n");
+        printf("ERROR: Is raw1394 module loaded ? Is your user in the 'disk' group ?\n");
+        printf("FAILURE.\n");
         return 1;
     }
-    printf("current generation number (driver): %d\n", raw1394_get_generation(handle));
+    printf("INFO: current generation number (driver): %d.\n", raw1394_get_generation(handle));
     // (void)
     raw1394_set_bus_reset_handler(handle, bus_reset_handler);
-    fprintf(stdout,"using adapter %d\n", adapter_number);
+    fprintf(stdout, "INFO: using adapter %d.\n", adapter_number);
     if (raw1394_set_port(handle, adapter_number) < 0) 
     {
-        printf("Could not set port");
+        printf("ERROR: Could not set port.\n");
+        printf("FAILURE.");
         return 1;
     }
     result = raw1394_reset_bus_new(handle, type);
     if (result == -1) 
     {
-        printf("Error trying to reset bus.\n");
+        printf("ERROR: Could not reset bus.\n");
+        printf("FAILURE.");
+        return 1;
     }
     else
     {
-        printf("Trying to reset bus did not result in an error.\nResult = %d\n", result);
-        //printf("(0 means success)\n");
+        // printf("INFO: Trying to reset bus did not result in an error.\nResult = %d\n", result);
+        // printf("(0 means success)\n");
     }
     raw1394_loop_iterate(handle);
     //usleep(1000);
