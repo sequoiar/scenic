33fa4fdcc1917825a0ae3a012c2fc184a28084c8
Koya Charles <koya.charles@gmail.com>
2009-04-13 18:27:22 +0000

    s//g error

--- src/gutil/queuePair.h
+++ src/gutil/queuePair.h
@@ -5,13 +5,13 @@
  *
  * This file is part of [propulse]ART.
  *
- * [propulse]ARMapMsg is free software: you can redistribute it and/or modify
+ * [propulse]ART is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * [propulse]ARMapMsg is distributed in the hope that it will be useful,
- * but WITHOUMapMsg ANY WARRANTY; without even the implied warranty of
+ * [propulse]ART is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
