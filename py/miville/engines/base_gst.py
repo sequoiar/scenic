919484ed6356ce2d82308de38e083a85e9523541
Hugo Boyer <ugomatik@gmail.com>
2009-04-14 20:41:34 +0000

    more loggin, for debuggin...

--- py/miville/engines/base_gst.py
+++ py/miville/engines/base_gst.py
@@ -31,7 +31,7 @@ from miville.protocols import ipcp
 from miville.utils import log
 from miville.utils.common import get_def_name
 
-log = log.start('info', 1, 0, 'base_gst')
+log = log.start('debug', 1, 0, 'base_gst')
 
 
 STOPPED = 0
@@ -98,9 +98,9 @@ class GstServer(object):
          log.info('GstServer state changed from %s to %s handle: %s ' % (all_states[old_state], all_states[new_state], str(self) ))
 
     def connection_ready(self, ipcp):
-        log.info("CONNECTION ready")
+        log.info("CONNECTION ready %s" % str(self))
         msg = 'GstServer.connection_ready: Address: %s, Port: %s, conn %s' % (self.address, self.port, str(ipcp) )
-        log.debug(msg)
+        log.info(msg)
         log.info('CONN: %s %s' % (ipcp, ipcp.__dict__))
         self.conn = ipcp
         self.conn.connectionLost = self.connection_lost
@@ -117,31 +117,32 @@ class GstServer(object):
         log.info('GST inter-process link created')
         
     def gst_video_init(self, **args):
-        log.info('GST VIDEO INIT acknowledged:  args %s' %  str(args) )
+        log.info('GST VIDEO INIT acknowledged:  args %s %s' %  (str(args), str(self))  )
         self.change_state(STREAMINIT)
 
     def gst_start(self,  **args):
-        log.info('GST START acknowledged... our args %s' % str(args) )
+        log.info('GST START acknowledged... our args %s  %s' % (str(args), str(self)) )
         self.change_state(STREAMING)
         
     def gst_stop(self,  **args):
-        log.info('GST STOP acknowledged ... our args %s' % str(args) )
+        log.info('GST STOP acknowledged ... our args %s  %s' % (str(args), str(self)) )
         self.change_state(STREAMSTOPPED)
 
     def gst_audio_init(self,  **args):
-        log.info('GST AUDIO INIT acknowledged our args %s' % str(args) )
+        log.info('GST AUDIO INIT acknowledged our args %s  %s' % (str(args), str(self)) )
         self.change_state(STREAMINIT)
 
     def gst_success(self, **args):
-        log.info('GST success :-)  args %s' %  str(args) )
+        log.info('GST success :-)  args %s %s' %  (str(args), str(self)) )
         self.change_state(STREAMINIT)
 
     def gst_failure(self, **args):
-        log.debug('GST failure :-(  args %s' %  str(args) )
+        log.debug('GST failure :-(  args %s %s' %  (str(args), str(self)) )
         self.change_state(STREAMINIT)
 
     def gst_log(self, level, msg):
-        msg = 'GstServer.gst_log: %s' % msg.partition(': ')[2].strip()
+        s = msg.partition(': ')[2].strip()
+        msg = 'GstServer.gst_log: %s %s' % (s, str(self))
         if level == 10:
             log.debug(msg)
         elif level == 30:
@@ -155,18 +156,18 @@ class GstServer(object):
 
 
     def connection_failed(self, conn):
-        msg = 'GstServer.connection_failed: Address: %s, Port: %s' % (self.address, self.port)
-        log.info(msg)
+        msg = 'GstServer.connection_failed: Address: %s, Port: %s %s' % (self.address, self.port, str(self))
+        log.info(msg + " "  + str(self))
         ipcp.connection_failed(conn)
         self.kill()
 
     def connection_lost(self, reason=protocol.connectionDone):
         self.kill()
         self.conn.del_callback('log')
-        log.info('Lost the server connection. Reason:\n%s' % reason)
+        log.info('Lost the server connection. Reason:\n%s %s' % (reason, str(self) ) )
 
     def start_process(self):
-        log.debug("GstServer.start_process...")
+        log.debug("GstServer.start_process... %s " % str(self) )
         #if self.state < RUNNING: self.state = RUNNING    # Uncomment this line to start the GST process "by hand"
         if self.state < STARTING:
             self.change_state(STARTING)
@@ -180,39 +181,39 @@ class GstServer(object):
                     protocol = GstProcessProtocol(self)
                     args = [path[0], mode_arg, "--serverport", str(self.port)]
                     proc_path = path[0]
-                    msg = "Start process> path: %s, protocol: %s args: %s" % (proc_path, protocol, str(args))
+                    msg = "Start process> path: %s, protocol: %s args: %s %s" % (proc_path, protocol, str(args), str(self))
                     # print msg
                     log.info(msg)
                     self.process = reactor.spawnProcess(protocol, proc_path, args, os.environ, usePTY=True)
-                    log.info("Process spawned")
+                    log.info("Process spawned pid=" + str(self.process.pid)  + " "+ str(self))
                     
                 except Exception, e:
-                    log.critical('Cannot start the GST application "%s": %s ' % (streaming_server_process_name, str(e)) )
+                    log.critical('Cannot start the GST application "%s": %s %s' % (streaming_server_process_name, str(e), str(self) ) )
             else:
-                log.critical('Cannot find the GST application: %s' % streaming_server_process_name)
+                log.critical('Cannot find the GST application: %s %s' % streaming_server_process_name, str(self) )
         else:
-            log.error("GstServer.start_process: not in proper state for starting")
+            log.error("GstServer.start_process: not in proper state for starting %s" % str(self))
 
     def kill(self):
-        log.debug("GstServer.kill")
+        log.debug("GstServer.kill %s" % str(self))
         self.change_state(STOPPED)
 #        os.kill(self._process.pid, signal.SIGTERM)
         reactor.callLater(0.5, self.verify_kill)
         try:
             self.process.loseConnection()
         except:
-            log.debug('Process %s or connection seems already dead.' % self.process.pid)
+            log.debug('Process %s or connection seems already dead. %s' % str(self.process.pid) ,str(self))
 #        self.process = None
 
     def verify_kill(self):
         try:
             status = os.waitpid(self.process.pid, 0)
         except:
-            log.debug('Process %s already dead' % self.process.pid)
+            log.debug('Process %s already dead %s %s' % (str(self.process.pid) , str(pid), str(self)) )
         else:
             if status[1] != 0:
                 pid = self.process.pid
-                log.debug("Killing pid: %s" % str(pid) )
+                log.debug("Killing pid: %s %s" % (str(pid)) )
                 try:
                     os.kill(pid, signal.SIGKILL)
                 except OSError:
@@ -224,7 +225,7 @@ class GstServer(object):
             if self.conn != None:
                 if self.state >= CONNECTED :
                     (cmd, args) = self.commands.pop()
-                    log.info('GstServer._process_cmd: ' + cmd + " args: " + str(args) )
+                    log.info('\n\n\nGstServer._process_cmd: \n  ' + cmd + "\n  args: " + str(args) + "\n  pid " + str(self.process.pid)  + " " + str(self) + '\n\n')
                     if args != None:
                         self.conn.send_cmd(cmd, *args)
                     else:
@@ -237,14 +238,14 @@ class GstServer(object):
         
 
     def send_cmd(self, cmd, args=None, callback=None, timer=None, timeout=3):
-        log.debug('GstServer.send_cmd state: ' + str(self.state) + " cmd: " +  cmd + " args: " + str(args))
+        log.debug('GstServer.send_cmd state: ' + str(self.state) + " cmd: " +  cmd + " args: " + str(args)+ "pid " + str(self.process.pid) + " " + str(self) )
         self.commands.insert(0, (cmd,args) )
         self._process_cmd()
 
 
 
     def del_callback(self, callback=None):
-        log.debug('GstServer.del_callback')
+        log.debug('GstServer.del_callback ' + str(self))
         if self.state == CONNECTED:
             if callback:
                 self.conn.del_callback(callback)
@@ -252,10 +253,10 @@ class GstServer(object):
                 try:
                     self.conn.del_callback(get_def_name())
                 except:
-                    log.debug("No callback to delete. (coming from: %s)." % get_def_name())
+                    log.debug("No callback to delete. (coming from: %s). %s" % (get_def_name(), str(self)))
 
     def add_callback(self, cmd, name=None, timer=None):
-        log.debug('GstServer.add_callback')
+        log.debug('GstServer.add_callback ' + str(self) )
         if self.conn:
             self.conn.add_callback(cmd, name)
         else:
@@ -267,7 +268,7 @@ class GstServer(object):
                 delay = 0.5
                 reactor.callLater(delay, self.add_callback, cmd, name, timer)
             else:
-                log.critical('The GST process cannot be ready to connect (from add_callback).')
+                log.critical('The GST process cannot be ready to connect (from add_callback).' + str(self))
 
 
 
