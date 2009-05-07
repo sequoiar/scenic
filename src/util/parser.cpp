9aeb49f8fae3d20a31cadf35f82e94d653c74ffe
Koya Charles <koya.charles@gmail.com>
2009-05-07 03:33:23 +0000

    Removed extranious log lines

--- src/util/parser.cpp
+++ src/util/parser.cpp
@@ -1,5 +1,4 @@
-/* parser.cpp
- * Copyright (C) 2008-2009 Société des arts technologiques (SAT)
+/* Copyright (C) 2008-2009 Société des arts technologiques (SAT)
  * http://www.sat.qc.ca
  * All rights reserved.
  *
@@ -122,7 +121,6 @@ bool MapMsg::tokenize(const std::string& str, MapMsg &cmd_map)
     std::string::size_type tok_end;
     std::string lstr = str;                                         //copy in str to local string
     cmd_map.clear();                                                //clear output map
-    LOG_DEBUG(str);
     tok_end = lstr.find_first_of(':');                              //search for ":"
     if(tok_end == lstr.size())                                      //if : not found return error
         THROW_ERROR("No command found.");
