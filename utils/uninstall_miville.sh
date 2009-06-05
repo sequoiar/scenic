0c747b1b77d83ae9d4bfbb4b4418f24bef2a1732
Alexandre Quessy <alexandre@quessy.net>
2009-06-05 17:47:29 +0000

    updated install scripts

--- utils/uninstall_miville.sh
+++ utils/uninstall_miville.sh
@@ -9,8 +9,11 @@ do
             exit
         ;;
         Y|y)
+            echo "sudo rm -rf /usr/local/lib/python2.5/site-packages/miville-0.1.3_a-py2.5.egg"
             sudo rm -rf /usr/local/lib/python2.5/site-packages/miville-0.1.3_a-py2.5.egg
+            echo "sudo rm -f /usr/local/bin/miville"
             sudo rm -f /usr/local/bin/miville
+            echo "sudo sed -i \"/miville/d\"  /usr/local/lib/python2.5/site-packages/easy-install.pth"
             sudo sed -i "/miville/d"  /usr/local/lib/python2.5/site-packages/easy-install.pth
             echo "Purged miville from /usr/local/"
             exit
