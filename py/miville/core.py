91c9281ded718a7776103a8615acb656af4a591c
Alexandre Quessy <alexandre@quessy.net>
2009-09-09 21:01:34 +0000

    Now miville doesnt write ~/.miville/miville.log when invoked with option --help.

--- py/miville/core.py
+++ py/miville/core.py
@@ -49,45 +49,6 @@ from miville import devices
 # module variables
 core = None
 
-class MivilleConfiguration(object):
-    """
-    Stores configuration options for miville. 
-    
-    Default values are set here.
-    They can be overriden using a dict passed to the constructor.
-    They can also be changed manually, simply. 
-    This class is not actually used yet.
-    """
-    def __init__(self, dictionary=None):
-        self.verbose = False # useless so far
-        # network
-        self.com_chan_port = 31054
-        self.telnet_port = 14444
-        self.restart_jackd = False
-        self.connector_port = 2222
-        self.web_port = 8080
-        self.iperf_port = 5001 # iperf's default
-        #self.midi_port = 44000
-        # self.ipcp_port = 999999999999
-        self.port_numbers_offset = 0
-        self.listen_to_interfaces = '' # means all interfaces
-        self.ui_network_interfaces = ['127.0.0.1'] # default is only local host
-        self.enable_escape_sequences = True
-        # files
-        self.miville_home = os.path.expanduser("~/.miville") # TODO: change for ~/.miville
-        self.addressbook_filename = 'addressbook.txt' # TODO: "contacts.txt"
-        self.settings_presets_filename = "presets.txt" # TODO: presets.txt
-        self.settings_filename = "settings.txt" # TODO: settings.txt
-        # update the attributes to match passed dict
-        if dictionary is not None:
-            self.update_dict(dictionary)
-
-    def update_dict(self, dico):
-        self.__dict__.update(dico)
-
-    def print_values(self):
-        for key, value in self.__dict__.items():
-            print "%20s: %s" % (key, value)
 
 class Core(Subject):
     """
