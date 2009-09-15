5f62ccc965b7d1be61d724dfd9cecf8808050969
Tristan Matthews <tristan@sat.qc.ca>
2009-09-15 15:30:25 +0000

    make caps fixed for raw > stereo

--- utils/make_caps.py
+++ utils/make_caps.py
@@ -127,8 +127,15 @@ for codec in ('raw', 'vorbis', 'mp3'):
         profile_name = codec + '_%d_%d' % (channels, rate)
         profiles[profile_name] = AudioProfile(encoders[codec], payloaders[codec], channels, rate)
 
-
 for profile_name, profile in profiles.iteritems():
     profile = generate_caps(profile_name, profile)
+
+# generate caps by hand for raw because its more stable than getting them from a pipeline
+codec = 'raw'
+for channels in xrange(3, 9):
+    profile_name = codec + '_%d_%d' % (channels, rate)
+    profiles[profile_name] = AudioProfile(encoders[codec], payloaders[codec], channels, rate)
+    profiles[profile_name].caps = 'application/x-rtp, media=(string)audio, clock-rate=(int)48000, encoding-name=(string)L16, encoding-params=(string)%d, channels=(int)%d, payload=(int)96' % (channels, channels)
+
     
 save_caps(profiles, Profile.filename)
