682744da5ebc7b9728545d60a74682b8be32c0e0
Alexandre Quessy <alexandre@quessy.net>
2010-01-27 22:26:34 +0000

    moved modules into package.

--- py/maugis/propulseart/runner.py
+++ py/maugis/propulseart/runner.py
@@ -18,8 +18,29 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Scenic. If not, see <http://www.gnu.org/licenses/>.
+
 """
-Executable script which calls propulseart.runner.run()
+Main of the application.
 """
-from propulseart import runner
-runner.run()
+from optparse import OptionParser
+
+try:
+    import pygtk
+    pygtk.require("2.0")
+except Exception, e:
+    print e
+try:
+    import gtk
+except ImportError, e:
+    print "Could not load GTK or glade. Install python-gtk2 and python-glade2.", str(e)
+    sys.exit(1)
+from propulseart import gui
+
+def run():
+    # command line parsing
+    parser = OptionParser(usage="%prog", version=str(gui.__version__))
+    parser.add_option("-k", "--kiosk", action="store_true", dest="kiosk", \
+            help="Run maugis in kiosk mode")
+    (options, args) = parser.parse_args()
+    app = gui.Application(kiosk=options.kiosk)
+    gtk.main()
