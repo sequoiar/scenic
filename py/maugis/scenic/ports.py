3a62ac44af496bcdc9a3d562bfc0f7eb6743e7d1
Alexandre Quessy <alexandre@quessy.net>
2010-02-01 14:56:28 +0000

    added port allocator. Not used yet.

---
+++ py/maugis/scenic/ports.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 
+# Scenic
+# Copyright (C) 2008 Société des arts technologiques (SAT)
+# http://www.sat.qc.ca
+# All rights reserved.
+#
+# This file is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Scenic is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Scenic. If not, see <http://www.gnu.org/licenses/>.
+"""
+Tools to help with choosing listening port numbers.
+"""
+
+# for port_is_avaiable
+import socket 
+
+class PortsAllocatorError(Exception):
+    """
+    Any error raised by the PortsAllocator
+    """
+    pass
+
+class PortsAllocator(object):
+    """
+    Allocates ports from a pool
+    """
+    def __init__(self, minimum=10000, increment=10, maximum=65535):
+        self.minimum = minimum
+        self.increment = increment
+        self.maximum = maximum
+        self.allocated = set()
+
+    def check_port(self, port):
+        """
+        Verify that port is available by trying to bind to it. Socket
+        does not persist. Note that some other process could still
+        bind to this port in between when this check happens and when
+        we bind to the port.
+        
+        :param port: int
+        Raises a PortsAllocatorError if port is not available.
+        """
+        # Set the socket parameters
+        host = 'localhost'
+        addr = (host, port)
+        busy = False
+
+        # Create socket and bind to address
+        udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        try:
+            udp_socket.bind(addr)
+        except socket.error, e:
+            busy = True
+        finally:
+            # Close socket
+            udp_socket.close()
+        if busy:
+            raise PortsAllocatorError("Port %d is not available. Reason:%s" % (port, e))
+        
+    def allocate(self):
+        """
+        Allocates a port number and returns it.
+        """
+        value = self.minimum
+        chosen = False
+        while not chosen:
+            while value in self.allocated: # loop over allocated ports
+                value += self.increment
+                if value > self.maximum:
+                    raise PortsAllocatorError("Maximum value reached. No more ports available.")
+            try:
+                self.check_port(value)
+            except PortsAllocatorError, e:
+                log.error(e.message) 
+            else:
+                chosen = True
+        self.allocated.add(value)
+        return value
+    
+    def free(self, value):
+        """
+        Frees an allocated port number.
+        Raises a PortsAllocatorError if not allocated.
+        """
+        if value not in self.allocated:
+            raise PortsAllocatorError("Value %d not in allocated ports set." % (value))
+        self.allocated.remove(value)
+
+        
+    def allocate_many(self, num=1):
+        """
+        Allocates many ports at once.
+        Returns a list of allocated ports.
+        """
+        ret = []
+        for i in range(num):
+            ret.append(self.allocate())
+        return ret
+    
+    def free_many(self, values):
+        """
+        Frees many allocated ports at a time.
+        :param values: list of integers.
+        """
+        for value in values:
+            self.free(value)
+
---
+++ py/maugis/scenic/test/test_ports.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+#
+# Miville
+# Copyright (C) 2008 Société des arts technologiques (SAT)
+# http://www.sat.qc.ca
+# All rights reserved.
+#
+# This file is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Miville is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Miville.  If not, see <http://www.gnu.org/licenses/>.
+
+from twisted.trial import unittest
+from scenic.ports import PortsAllocator
+from scenic.ports import PortsAllocatorError
+
+class Test_01_Ports_Allocator(unittest.TestCase):
+    def _tst(self, expected, value):
+        if value != expected:
+            self.fail("Expected value %s but got %s." % (expected, value))
+
+    def test_01_add_remove(self):
+        a = PortsAllocator(minimum=2000, increment=2, maximum=2010)
+
+        # value = 2000; value < 2012; value += 2
+        for value in xrange(2000, 2012, 2):
+            self._tst(value, a.allocate())
+        try:
+            value = a.allocate()
+        except PortsAllocatorError, e:
+            pass
+        else:
+            self.fail("Ports allocator should have overflown. Got value %d." % (value))
+        # value = 2000; value < 2012; value += 2
+        for value in xrange(2000, 2012, 2):
+            a.free(value)
+        try:
+            a.free(100)
+        except PortsAllocatorError, e:
+            pass
+        else:
+            self.fail("Trying to free value %d should have raised an error." % (100))
+            
+    def test_02_add_many(self):
+        a = PortsAllocator(minimum=2000, increment=2, maximum=2010)
+        values = a.allocate_many(6)
+        a.free_many(values)
+        values = a.allocate_many(3)
+        values = a.allocate_many(3)
+        try:
+            value = a.allocate()
+        except PortsAllocatorError, e:
+            pass
+        else:
+            self.fail("Ports allocator should have overflown. Got value %d." % (value))
+            pass
+
