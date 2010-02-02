34e654f693801b04ac9c64753f2080de0c2ab4f6
Alexandre Quessy <alexandre@quessy.net>
2010-02-02 16:38:37 +0000

    adding GTK dialogs and test_signals.py

---
+++ py/maugis/scenic/test/test_signals.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from twisted.trial import unittest
+from scenic import sig
+from twisted.internet import reactor
+from twisted.internet import defer
+
+class Test_01_Method_Signals(unittest.TestCase):
+    def _slot(self):
+        self.called = True
+    def test_methods(self):
+        self.called = False
+        new_signal = sig.Signal()
+        new_signal.connect(self._slot)
+        new_signal()
+        if not self.called:
+            self.fail("Slot hasn't been called.")
+
+    def test_args(self):
+        pass
+    test_args.skip = "to do"
+
+    def test_kwargs(self):
+        pass
+    test_kwargs.skip = "to do"
+
+class Test_02_Function_Signals(unittest.TestCase):
+    def test_function(self):
+        pass
+    test_function.skip = "Our Pure-Python signals don't work with functions yet."
