c086643b6477ece0bc0698a3ec495a9fb72b901d
Alexandre Quessy <alexandre@quessy.net>
2010-02-08 19:14:47 +0000

    added devices/x11 and its test.

--- py/maugis/scenic/devices/x11.py
+++ py/maugis/scenic/devices/x11.py
@@ -1,6 +1,7 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Miville
+# 
+# Scenic
 # Copyright (C) 2008 Société des arts technologiques (SAT)
 # http://www.sat.qc.ca
 # All rights reserved.
@@ -10,13 +11,13 @@
 # the Free Software Foundation, either version 2 of the License, or
 # (at your option) any later version.
 #
-# Miville is distributed in the hope that it will be useful,
+# Scenic is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Miville.  If not, see <http://www.gnu.org/licenses/>.
+# along with Scenic. If not, see <http://www.gnu.org/licenses/>.
 """
 X11 driver. Used to list X11 displays/screens.
 
@@ -25,57 +26,10 @@ Uses xpyinfo, looping until no more screen/display is found. ::
   xdpyinfo -display :0.0
 """
 import subprocess
+from twisted.internet import threads
 from twisted.internet import reactor
 
-from miville.devices import devices #FIXME: ugly
-from miville.utils import log as logger
-from miville.utils import commands
-from miville.errors import CommandNotFoundError
-
-log = logger.start('info', True, True, 'x11')
-
-class X11Driver(devices.VideoDriver):
-    """
-    Miville X11 displays driver
-    """
-    name = 'x11'
-    command_found = False
-    
-    def prepare(self):
-        """
-        Returns a Deferred instance? no
-        """
-        try:
-            tmp = commands.find_command('xdpyinfo')
-        except CommandNotFoundError, e:
-            #self.api.notify(None, "xdpyinfo command not found. Is xorg installed ?", "error")
-            log.error("xdpyinfo command not found. Is xorg installed ?")
-            raise # catched in devices.py
-        else:
-            self.command_found = True
-        return devices.Driver.prepare(self)
-    
-    def _on_devices_polling(self, caller=None, event_key=None):
-        """
-        Get all infos for all devices.
-        
-        Overrides devices.Device._on_devices_polling
-        Must return a Deferred instance.
-        """
-        if self.command_found:
-            displays = _list_x11_displays()
-            for display in displays:
-                log.debug("Adding display %s" % (display["name"]))
-                d = devices.Device(display['name'])
-                d.add_attribute(devices.StringAttribute("resolution", display["resolution"]))
-                d.add_attribute(devices.StringAttribute("dimensions", display["dimensions"]))
-                self._add_new_device(d)
-        self._on_done_devices_polling(caller, event_key)
-
-    def on_attribute_change(self, attr, caller=None, event_key=None):
-        raise DeviceError("It is not possible to change x11 devices attributes.")
-
-def _list_x11_displays():
+def _list_x11_displays(verbose):
     """
     Returns a list of X11 display/screen names.
     
@@ -89,7 +43,8 @@ def _list_x11_displays():
     displays = []
     for i in range(10):
         display = ":%d.0" % (i)
-        log.debug("calling xdpyinfo -display %s" % (display))
+        if verbose:
+            print("calling xdpyinfo -display %s" % (display))
         command = "xdpyinfo -display %s" % (display)
         output = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=dev_null).communicate()[0]
         found_screen_for_display = False
@@ -104,23 +59,25 @@ def _list_x11_displays():
                 found_screen_for_display = True
                 screen = ":%d.%d" % (i, j)
                 displays.append({"name":screen, "dimensions":dimen, "resolution":resolution})
-                log.debug("Found X11 display and screen %s with dimensions = %s" % (screen, dimen))
+                if verbose:
+                    print("Found X11 display and screen %s with dimensions = %s" % (screen, dimen))
         if not found_screen_for_display:
             break
     dev_null.close()
     return displays
 
-def start(api):
+def list_x11_displays(verbose=True):
     """
-    Starts this driver.
-    
-    Called from the core.
+    Twisted wrapper for _list_x11_displays.
+    Result is a list of dicts with keys 'name', 'dimensions', 'resolution'.
+    @rettype: Deferred
     """
-    driver = X11Driver()
-    driver.api = api
-    log.info("Starting the X11 driver.")
-    devices.managers['video'].add_driver(driver)
-    reactor.callLater(0, driver.prepare)
+    return threads.deferToThread(_list_x11_displays, verbose)
 
 if __name__ == "__main__":
-    print(_list_x11_displays())
+    def _cb(result):
+        print "result:", result
+        reactor.stop()
+    d = list_x11_displays()
+    d.addCallback(_cb)
+    reactor.run()
---
+++ py/maugis/scenic/test/test_x11.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Unit tests for the X11 function.
+""" 
+from twisted.trial import unittest
+from scenic.devices import x11
+
+class Test_X11(unittest.TestCase):
+    def test_list_displays(self):
+        def _cb(result):
+            if len(result) < 1:
+                self.fail("There should be at least one X11 display listed.")
+            return result
+        deferred = x11.list_x11_displays(verbose=False)
+        deferred.addCallback(_cb)
+        return deferred
