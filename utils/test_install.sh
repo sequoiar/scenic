4e9c9a11b00c5782ede5c5369cc0820052948ead
Tristan Matthews <tristan@sat.qc.ca>
2010-02-11 23:52:28 +0000

    dist not quite there yet

--- utils/test_install.sh
+++ utils/test_install.sh
@@ -6,15 +6,20 @@ PYTHON_LIB=$(python -E -c 'import sys; print("python%d.%d" % sys.version_info[0:
 
 export PYTHONPATH=$INSTALL_ROOT/$_PREFIX/lib/$PYTHON_LIB/site-packages
 
+make distclean
 if [ ! -x ./autogen.sh ]
 then
     echo "No ./autogen.sh script available" >&2
-    echo "Quitting" >&2
-    exit 1
-fi
+    if [ ! -x ./configure ]
+    then
+        echo "No ./configure script available" >&2
+    else
+        ./configure
+    fi
+else
+    ./autogen.sh
+fi &&
 
-make distclean
-./autogen.sh &&
 make &&
 make DESTDIR=$INSTALL_ROOT install
 
