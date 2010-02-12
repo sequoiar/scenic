fdd439bb5ea3d5c7ab153df5853c2a34bf634116
Tristan Matthews <tristan@sat.qc.ca>
2010-02-12 23:32:24 +0000

    distcheck still not working

--- utils/install_miville.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "This script must be run in the miville/trunk/py directory."
-echo "Installing miville in /usr/local..."
-echo "python setup.py clean"
-python setup.py clean
-echo "python setup.py build"
-python setup.py build
-echo "sudo python setup.py install --prefix=/usr/local"
-sudo python setup.py install --prefix=/usr/local
