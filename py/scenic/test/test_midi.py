c9895ea1bdb17da340f0d79e6b343748bb1680a5
Alexandre Quessy <alexandre@quessy.net>
2010-03-11 00:00:11 +0000

    added test_midi.py

---
+++ py/scenic/test/test_midi.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Unit tests for the X11 function.
+""" 
+from twisted.trial import unittest
+from scenic.devices import midi
+
+#TODO: this test could test more
+
+class Test_Midi(unittest.TestCase):
+    def test_list_midi_devices(self):
+        def _cb(result):
+            print result
+            return result
+        deferred = midi.list_midi_devices()
+        deferred.addCallback(_cb)
+        return deferred
