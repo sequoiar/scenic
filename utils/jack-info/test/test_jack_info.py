229c7c65dd94882d340d5a6291d9b1e61f73f2f3
Tristan Matthews <tristan@sat.qc.ca>
2010-03-17 18:29:25 +0000

    test ready

--- utils/jack-info/test/test_jack_info.py
+++ utils/jack-info/test/test_jack_info.py
@@ -4,7 +4,7 @@ from twisted.trial import unittest
 # Make sure TESTS_ENVIRONMENT has jack-info in path (shouldn't need to be installed)
 
 class TestJackInfo(unittest.TestCase):
-    def test_jack_info_help(self):
+    def test_jack_info(self):
         """
             Check that jack-info prints backend, device, name, nperiods, period, pid, rate
         """
@@ -15,5 +15,18 @@ class TestJackInfo(unittest.TestCase):
                 stdout=subprocess.PIPE)
 
         stdout_value = proc.communicate()[0]
-        print "value is: \n"
-        print stdout_value
+        print 'value is: \n'
+        info = {}
+        # parse output of jack-info into dict (stripping the ":" from the key name
+        for line in stdout_value.splitlines():
+            key = line.split()[0][0:-1]
+            value = line.split()[-1]
+            info[key] = value
+            print "%s: %s" % (key, str(value))
+
+        expected = ['backend', 'device', 'name', 'nperiods', 'period', 'pid', 'rate']
+        for key in expected:
+            assert(key in info.iterkeys())
+        
+        # check for correct number of lines
+        assert(len(info) == len(expected))
