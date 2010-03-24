1b8bdc8d7d614c9e03b9c4d582ee7fb16148ec75
Tristan Matthews <tristan@sat.qc.ca>
2010-03-24 18:06:09 +0000

    Added test skeleton

---
+++ src/vumeter/test/test_vumeter.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from twisted.trial import unittest
+
+class TestVumeter(unittest.TestCase):
+    def test_vumeter(self):
+        """
+            Check that we can roll vumeter
+        """
+        assert(True)
