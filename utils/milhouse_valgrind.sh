b1b5a5273784eb3229efb3212af8eb909d87e6c3
Tristan Matthews <tristan@sat.qc.ca>
2010-04-08 19:31:29 +0000

    better options for valgrind script

--- utils/milhouse_valgrind.sh
+++ utils/milhouse_valgrind.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-G_SLICE=always-malloc G_DEBUG=gc-friendly libtool --mode=execute valgrind --tool=memcheck --leak-check=full --leak-resolution=high --num-callers=20 --log-file=vgdump milhouse $@
+# run 
+# ln -s gstreamer-0.10-*/common/gst.supp .
+G_SLICE=always-malloc G_DEBUG=gc-friendly libtool --mode=execute \ 
+valgrind --log-file=vgdump --tool=memcheck --leak-check=full --leak-resolution=high \
+--trace-children=yes --num-callers=20 --suppressions=gst.supp -v milhouse $@
