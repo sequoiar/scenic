40e520e7ca0a8367992834bdbfb7ea0b8a6203f9
Tristan Matthews <tristan@sat.qc.ca>
2010-04-09 15:59:48 +0000

    need local audio in svn

---
+++ src/gst/localAudio.h
@@ -0,0 +1,50 @@
+
+// localVideo.h
+// Copyright (C) 2008-2009 Société des arts technologiques (SAT)
+// http://www.sat.qc.ca
+// All rights reserved.
+//
+// This file is part of [propulse]ART.
+//
+// [propulse]ART is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// [propulse]ART is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with [propulse]ART.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#ifndef _LOCAL_AUDIO_H_
+#define _LOCAL_AUDIO_H_
+
+#include "audioConfig.h"
+#include "noncopyable.h"
+
+#include <boost/shared_ptr.hpp>
+
+class AudioSource;
+class AudioLevel;
+class _GstElement;
+
+class LocalAudio : boost::noncopyable
+{
+    public:
+        LocalAudio(Pipeline &pipeline, boost::shared_ptr<AudioSourceConfig> sourceConfig);
+        ~LocalAudio();
+
+    private:
+        Pipeline &pipeline_;
+        boost::shared_ptr<AudioSourceConfig> sourceConfig_;
+        AudioSource *source_;
+        AudioLevel *level_;
+        _GstElement *fakesink_;
+};
+
+#endif // _LOCAL_AUDIO_H_
+
