61a3911e8b566d3284b5e19ebd7145407f0b3e43
Tristan Matthews <tristan@sat.qc.ca>
2010-04-12 21:34:01 +0000

    fixed preview in branches/buffer

--- py/scenic/preview.py
+++ py/scenic/preview.py
@@ -44,7 +44,7 @@ class Preview(object):
                 x_window_id = self.app.gui.preview_area_x_window_id
         command = "milhouse --videosource %s --localvideo --window-title \"%s\" --width %s --height %s --aspect-ratio %s" % (self.app.config.video_source, window_title, width, height, aspect_ratio, )
         if self.app.devices["jackd_is_running"]: 
-            command += " --localaudio --numchannels %s --vumeter-id %s --audio-buffer" % (numchannels, vumeter_id, audio_buffer)
+            command += " --localaudio --numchannels %s --vumeter-id %s --audio-buffer %s" % (numchannels, vumeter_id, audio_buffer)
         #else:
         #    warning_message = "You should consider starting jackd."
         if x_window_id is not None:
