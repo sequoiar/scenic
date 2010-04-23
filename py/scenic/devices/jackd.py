e18e9cadd1c00d343b71b07ee4eb5c7a17ea3fe6
Alexandre Quessy <alexandre@quessy.net>
2010-04-23 13:50:24 +0000

    hmmmm

--- py/scenic/devices/jackd.py
+++ py/scenic/devices/jackd.py
@@ -108,7 +108,7 @@ def jackd_get_infos():
         except ValueError:
             pass
         else:
-            ret[i] = {}
+            ret.append({})
             ret[i]['name'] = name # probably 'default' or $JACK_DEFAULT_SERVER
             ret[i]['pid'] = pid
             # adding some default values!! XXX
