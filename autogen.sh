958e9a1fa47e725362a5233b63ff6eca8e016ba2
Alexandre Quessy <alexandre@quessy.net>
2010-05-14 20:00:29 +0000

    barf if aclocal returns an error. Added a check for a gst element - the modern way.

--- autogen.sh
+++ autogen.sh
@@ -21,6 +21,11 @@ fi
 
 # could be replaced with autoreconf -fivI m4 (verbose, force rebuild of ltmain, .in files, etc.)
 autoreconf --install
+
+if [ $! != 0 ]; then 
+    exit 1
+fi
+
 if [ ! "x$LOGNAME" = "xbbslave" ]; then
     ./configure $@ --enable-svn-revision
 fi
