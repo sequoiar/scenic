a13a301dda717984e77863d07efb26f29e83d2e3
Alexandre Quessy <alexandre@quessy.net>
2011-03-21 12:58:30 -0400

    expand docbook path with LANG environment variable

--- py/scenic/gui.py
+++ py/scenic/gui.py
@@ -1562,13 +1562,28 @@ class Gui(object):
         """
         log.info("Menu item 'Quit' chosen")
         self._confirm_and_quit()
+
+    def _expand_docbook_path_with_locale(self):
+        """
+        Appends the locale dir name to docbook path if found.
+        """
+        lang = "en"
+        try:
+            lang = os.environ["LANG"].split("_")[0]
+        except KeyError, e:
+            log.error('Key %s is missing in environment variables' % (e))
+        path = os.path.join(configure.DOCBOOK_DIR, lang)
+        if os.path.isdir(path):
+            return path
+        else:
+            return os.path.join(configure.DOCBOOK_DIR, "en")
     
     def on_installation_manual_menu_item_activated(self, unused_menu_item):
         """
         Opens the docbook doc
         """
         log.info("Menu item 'Installatin manual' chosen")
-        docbook_file = os.path.join(configure.DOCBOOK_DIR, "installation-manual.xml")
+        docbook_file = os.path.join(self._expand_docbook_path_with_locale(), "installation-manual.xml")
         process.run_once("yelp", docbook_file)
     
     def on_help_menu_item_activated(self, unused_menu_item):
@@ -1576,7 +1591,7 @@ class Gui(object):
         Opens the docbook doc
         """
         log.info("Menu item 'User manual' chosen")
-        docbook_file = os.path.join(configure.DOCBOOK_DIR, "user-manual.xml")
+        docbook_file = os.path.join(self._expand_docbook_path_with_locale(), "user-manual.xml")
         process.run_once("yelp", docbook_file)
 
     def on_status_menu_item_activated(self, unused_menu_item):
